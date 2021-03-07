6576ae13f3b3445d961b34940d4e83d1867b4cd5
thedaebu <min.kim163@gmail.com>
2021-03-07 17:41:29 -0500

    initial commit

---
+++ frontend/entry.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root'
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+
+  //store is provided to the root as a prop
+  ReactDOM.render(<Root store={store} />, root);
+
+  const store = configureStore();
+  window.store = store;
+  
+});
