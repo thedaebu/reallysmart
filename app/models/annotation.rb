167c2af3842f08b6b7532ebd1e77a57c4563129f
thedaebu <min.kim163@gmail.com>
2021-03-23 19:15:23 -0400

    model bug fix

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -9,7 +9,7 @@ class Annotation < ApplicationRecord
         foreign_key: :annotator_id,
         class_name: "User"
 
-    belongs_to :track
+    belongs_to :track,
         foreign_key: :track_id,
         class_name: "Track"
 
