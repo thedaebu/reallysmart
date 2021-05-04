31690525a076b0e2ca103ecd7a298bf1f18d8d53
thedaebu <min.kim163@gmail.com>
2021-05-04 14:44:06 -0400

    add searches to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import annotationsReducer from './annotations_reducer';
 import commentsReducer from './comments_reducer';
+import searchesReducer from './searches_reducer';
 import tracksReducer from './tracks_reducer';
 import usersReducer from './users_reducer';
 import votesReducer from './votes_reducer';
@@ -10,7 +11,8 @@ const entitiesReducer = combineReducers({
     tracks: tracksReducer,
     annotations: annotationsReducer,
     comments: commentsReducer,
-    votes: votesReducer
+    votes: votesReducer,
+    searches: searchesReducer
 });
 
 export default entitiesReducer;
