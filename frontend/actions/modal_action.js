ecbc27a0670f0f803f6fc8e2680fb1d694bfad59
thedaebu <min.kim163@gmail.com>
2021-05-04 14:33:32 -0400

    edit modal actions

--- frontend/actions/modal_action.js
+++ frontend/actions/modal_action.js
@@ -5,11 +5,11 @@ export const openModal = data => {
     return ({
         type: OPEN_MODAL,
         data
-    })
-}
+    });
+};
 
 export const closeModal = () => {
     return ({
         type: CLOSE_MODAL    
-    })
-}
+    });
+};
