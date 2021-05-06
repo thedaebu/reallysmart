2437fecd4eed6c45f2838eddceefe1b7f4f131d8
thedaebu <min.kim163@gmail.com>
2021-05-05 21:54:20 -0400

    add searchbar items frontend code

--- frontend/components/searchbar/searchbar.jsx
+++ frontend/components/searchbar/searchbar.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 
 class Searchbar extends React.Component {
@@ -12,6 +19,10 @@ class Searchbar extends React.Component {
         this.onChange = this.onChange.bind(this);
     }
 
+    randomNum() {
+        return Math.floor(Math.random() * 1000);
+    }
+
     onChange() {
         if (this.state.searchField !== "") {
             this.props.fetchSearches(this.state.searchField);
@@ -20,6 +31,39 @@ class Searchbar extends React.Component {
     }
 
     render() {
+        const { searches } = this.props;
+
+        let searchbarItems;
+        if (searches) {
+            searchbarItems = searches.slice(0, 5).map(searchbarItem => {
+                return (
+                    <Link className='searchbar-item' to={`/tracks/${searchbarItem.id}`}>
+                        <div className='searchbar-item-image' style={{ background: `url(${searchbarItem.artwork_path}`, backgroundPosition: 'center', backgroundSize: 'cover' }}> </div>
+                        <div className='searchbar-item-right'>
+                            <div className='searchbar-item-right-top'>
+                                <p className='searchbar-item-title'>{searchbarItem.title}</p>
+                                <p className='searchbar-item-artist'>{searchbarItem.artist}</p>
+                            </div>
+                            <div className='searchbar-item-right-bottom'>
+                                <img className='track-index-item-eye' src={window.eyeIcon} />
+                                <p className='searchbar-item-views'>{this.randomNum()}</p>
+                            </div>
+                        </div>
+                    </Link>
+                );
+            })
+        }
+
+        let searchbarModal;
+        if (searches) {
+            searchbarModal = 
+                <div className='searchbar-items-modal'>
+                    <ul className='searchbar-items-list'>
+                        {searchbarItems}
+                    </ul>
+                </div>
+        }
+
         return (
             <div>
                 <div className='search-bar-main'>
@@ -31,7 +75,7 @@ class Searchbar extends React.Component {
                     />
                     <AiOutlineSearch className='search-bar-glass' />
                 </div>   
-                       
+                {searchbarModal}
             </div>
         )
     }
--- frontend/components/searchbar/searchbar_container.js
+++ frontend/components/searchbar/searchbar_container.js
@@ -4,7 +4,7 @@ import Searchbar from "./searchbar";
 
 const mSTP = ( state, ownProps ) => {
     return ({
-
+        searches: state.props.entities.searches
     });
 };
 
