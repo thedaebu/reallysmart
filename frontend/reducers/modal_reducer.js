28122878d075f763588ec7e2e96877f993680e9b
thedaebu <min.kim163@gmail.com>
2021-05-12 18:42:16 -0400

    refactor modal reducer to remove searchbar modal

--- frontend/reducers/modal_reducer.js
+++ frontend/reducers/modal_reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 import annotationModalReducer from "./annotation_modal_reducer";
-import searchbarModalReducer from "./searchbar_modal_reducer";
 
 const modalReducer = combineReducers({
-    searchbarModal: searchbarModalReducer,
     annotationModal: annotationModalReducer
 });
 
