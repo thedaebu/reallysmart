a4857fe4f717661e00562153f52576c7082cae24
thedaebu <min.kim163@gmail.com>
2021-05-12 18:45:15 -0400

    refactor actions reducer

--- frontend/reducers/searches_reducer.js
+++ frontend/reducers/searches_reducer.js
@@ -1,4 +1,4 @@
-import { CLEAR_SEARCHES, RECEIVE_SEARCHES } from "../actions/search_actions";
+import { RECEIVE_SEARCHES } from "../actions/search_actions";
 
 const searchesReducer = (state = {}, action) => {
     Object.freeze(state);
@@ -6,8 +6,6 @@ const searchesReducer = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_SEARCHES:
             return Object.assign({}, action.searches);
-        case CLEAR_SEARCHES:
-            return (Object.assign({}));
         default:
             return state;
     }
