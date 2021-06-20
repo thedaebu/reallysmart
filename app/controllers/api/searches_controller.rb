0f04ad18eade7ade3c81e33c10a624075fff8fcb
thedaebu <min.kim163@gmail.com>
2021-06-19 21:18:51 -0400

    edit searches_controller.rb for better search functionality

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,6 +1,18 @@
 class Api::SearchesController < ApplicationController
     def index
         if params[:search]
+            # if params[:search].includes?(" ")
+            #     tags = []
+            #     new_search = params[:search].split(" ")
+            #     new_search.each do |name_search|
+            #         tag = Tag.where("lower(name) LIKE ?", "%#{name_search}%")
+            #         tags.push(tag)
+            #     end
+            # else
+            #     name_search = params[:search]
+            #     tags = Tag.where("lower(name) LIKE ?", "%#{name_search}%")
+            # end
+            
             name_search = params[:search]
             tags = Tag.where("lower(name) LIKE ?", "%#{name_search}%")
             @searches = tags
