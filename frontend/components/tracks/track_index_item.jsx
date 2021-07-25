2ea8c6ea3c3dac7c54e28c5e04866c436174c840
thedaebu <min.kim163@gmail.com>
2021-07-24 21:29:04 -0400

    edit for neatness for frontend/components/tracks/track_index_item.jsx

--- frontend/components/tracks/track_index_item.jsx
+++ frontend/components/tracks/track_index_item.jsx
@@ -21,22 +21,22 @@ class TrackIndexItem extends React.Component {
       
         return ( 
             <Link className="track-index-item" to={`/tracks/${track.id}`}>        
-                <p className='track-index-item-id'>{track.id}</p>
-                <div className='track-index-item-image' style={{ background: `url(${track.artwork_path}`, backgroundPosition: 'center', backgroundSize: 'cover' }}> </div>
+                <p className="track-index-item-id">{track.id}</p>
+                <div className="track-index-item-image" style={{ background: `url(${track.artwork_path}`, backgroundPosition: "center", backgroundSize: "cover" }}> </div>
                 
-                <div className='track-index-item-title-div'>
-                    <p className='track-index-item-title'>{track.title}</p>
-                    <p className='track-index-item-lyrics'>LYRICS</p>
+                <div className="track-index-item-title-div">
+                    <p className="track-index-item-title">{track.title}</p>
+                    <p className="track-index-item-lyrics">LYRICS</p>
                 </div>
 
-                <p className='track-index-item-artist'>{track.artist}</p>
-                <div className='track-index-item-fire-main'>
-                    <img className='track-index-item-fire' src={window.fireIcon} />
-                    <p className='track-index-item-fire-number'>{this.randomNum()}</p>
+                <p className="track-index-item-artist">{track.artist}</p>
+                <div className="track-index-item-fire-main">
+                    <img className="track-index-item-fire" src={window.fireIcon} />
+                    <p className="track-index-item-fire-number">{this.randomNum()}</p>
                 </div>
-                <div className='track-index-item-eye-main'>
-                    <img className='track-index-item-eye' src={window.eyeIcon} />
-                    <p className='track-index-item-eye-number'>{this.randomNum() * 10}</p>
+                <div className="track-index-item-eye-main">
+                    <img className="track-index-item-eye" src={window.eyeIcon} />
+                    <p className="track-index-item-eye-number">{this.randomNum() * 10}</p>
                 </div>
             </Link>
         );
