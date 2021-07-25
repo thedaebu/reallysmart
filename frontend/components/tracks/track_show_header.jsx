b247e3b20de7c484866b598475a5f574ff3e54c5
thedaebu <min.kim163@gmail.com>
2021-07-24 21:32:50 -0400

    edit for neatness for frontend/components/tracks/track_show_header.jsx

--- frontend/components/tracks/track_show_header.jsx
+++ frontend/components/tracks/track_show_header.jsx
@@ -9,30 +9,29 @@ class TrackShowHeader extends React.Component {
       return Math.floor(Math.random() * 1000);
     }
 
-
     render() {
         const { track } = this.props;
         const art = this.props.track.artwork_path;
 
         return (
-            <div className="track-show-header-main" style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${art})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
-                <div className='track-show-header-shade'>
-                    <div className='track-show-header-content'>
-                        <div className='track-show-header-left'>
-                            <div className='track-show-header-image' style={{ backgroundImage: `url(${art}`, backgroundPosition: 'center', backgroundSize: 'cover' }} ></div>
-                            <div className='track-show-header-center'>
-                                <p className='track-show-header-title' >{track.title}</p>
-                                <p className='track-show-header-artist'>{track.artist}</p>
+            <div className="track-show-header-main" style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${art})`, backgroundPosition: "center", backgroundSize: "cover" }}>
+                <div className="track-show-header-shade">
+                    <div className="track-show-header-content">
+                        <div className="track-show-header-left">
+                            <div className="track-show-header-image" style={{ backgroundImage: `url(${art}`, backgroundPosition: "center", backgroundSize: "cover" }} ></div>
+                            <div className="track-show-header-center">
+                                <p className="track-show-header-title" >{track.title}</p>
+                                <p className="track-show-header-artist">{track.artist}</p>
                             </div>
                         </div>
-                        <div className='track-show-header-right'>
-                            <div className='track-index-item-fire-main'>
-                                <img className='track-index-item-fire' src={window.fireIcon} />
-                                <p className='track-index-item-fire-number'>{this.randomNum()}</p>
+                        <div className="track-show-header-right">
+                            <div className="track-index-item-fire-main">
+                                <img className="track-index-item-fire" src={window.fireIcon} />
+                                <p className="track-index-item-fire-number">{this.randomNum()}</p>
                             </div>
-                            <div className='track-index-item-eye-main'>
-                                <img className='track-index-item-eye' src={window.eyeIcon} />
-                                <p className='track-index-item-eye-number'>{this.randomNum() * 10}</p>
+                            <div className="track-index-item-eye-main">
+                                <img className="track-index-item-eye" src={window.eyeIcon} />
+                                <p className="track-index-item-eye-number">{this.randomNum() * 10}</p>
                             </div>
                         </div>
                     </div>
