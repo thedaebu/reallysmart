ec51e6e75aaaf638b87bbba2038500d9d68d93c4
thedaebu <min.kim163@gmail.com>
2021-10-23 21:27:10 -0400

    change track_index.js to use react hooks

--- frontend/components/tracks/track_index.jsx
+++ frontend/components/tracks/track_index.jsx
@@ -1,35 +1,26 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Navbar from "../header/navbar";
 import TrackIndexItem from "./track_index_item";
 
-class TrackIndex extends React.Component {
-    constructor(props) {
-        super(props);
+function TrackIndex(props) {
+    const [list, setList] = useState(5);
 
-        this.state = {
-            list: 5
-        };
-
-        this.trackIndexItems = this.trackIndexItems.bind(this);
-        this.listButton = this.listButton.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    componentDidMount() {   
-        this.props.fetchTracks();    
+    useEffect(() => {
+        debugger
+        props.fetchTracks();
         window.scrollTo(0, 0);
-    }
+    }, "")
 
-    trackIndexItems() {
-        const { tracks } = this.props;
+    function trackIndexItems() {
+        const { tracks } = props;
 
-        if (this.state.list === 5) {
+        if (list === 5) {
             return (
                 tracks.slice(0, 5).map(track => {
                     return <TrackIndexItem track={track} key={track.id} />;
                 })
             );
-        } else if (this.state.list === 10) {
+        } else if (list === 10) {
             return (
                 tracks.slice(0, 10).map(track => {
                     return <TrackIndexItem track={track} key={track.id} />;
@@ -44,14 +35,14 @@ class TrackIndex extends React.Component {
         }
     }
 
-    listButton() {
-        if (this.state.list === 5) {
+    function listButton() {
+        if (list === 5) {
             return ( 
-                <p onClick={this.handleClick} className="tracks-index-load-more">LOAD MORE</p>
+                <p onClick={handleClick} className="tracks-index-load-more">LOAD MORE</p>
             );
-        } else if (this.state.list === 10) { 
+        } else if (list === 10) { 
             return ( 
-                <p onClick={this.handleClick} className="tracks-index-load-more">We Miss You DMX!</p>
+                <p onClick={handleClick} className="tracks-index-load-more">We Miss You DMX!</p>
             );
         } else {
             return (
@@ -60,35 +51,29 @@ class TrackIndex extends React.Component {
         }
     }
 
-    handleClick(e) {
+    function handleClick(e) {
         e.preventDefault();
 
-        if (this.state.list === 5) {
-            this.setState({
-                list: 10
-            });
-        } else if (this.state.list === 10) {
-            this.setState({
-                list: 11
-            });
+        if (list === 5) {
+            setList(10);
+        } else if (list === 10) {
+            setList(11);
         }
     }
 
-    render() {
-        return (
-            <div className="tracks-index-main">
-                <Navbar />
-                <div className="tracks-index-top">
-                    <h1 >CHARTS</h1>
-                    <h2 >REALLY POPULAR ON REALLY SMART</h2>
-                    <ul >
-                        {this.trackIndexItems()}
-                    </ul>
-                </div>
-                {this.listButton()}
+    return (
+        <div className="tracks-index-main">
+            <Navbar />
+            <div className="tracks-index-top">
+                <h1 >CHARTS</h1>
+                <h2 >REALLY POPULAR ON REALLY SMART</h2>
+                <ul >
+                    {trackIndexItems()}
+                </ul>
             </div>
-        );
-    }
+            {listButton()}
+        </div>
+    );
 };
 
 export default TrackIndex;
