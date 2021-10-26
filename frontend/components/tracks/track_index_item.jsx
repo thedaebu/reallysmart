9315dd2a3dac417fe3c0da8dfd459b6ed66b4f7e
thedaebu <min.kim163@gmail.com>
2021-10-25 20:10:47 -0400

    reformat for react hooks

--- frontend/components/tracks/track_index_item.jsx
+++ frontend/components/tracks/track_index_item.jsx
@@ -7,38 +7,40 @@ import {
     HashRouter
 } from "react-router-dom";
 
-class TrackIndexItem extends React.Component {
-    constructor(props) {
-        super (props);
-    }
-    
-    randomNum() {
+function TrackIndexItem(props) {
+    const { track } = props; 
+
+    function randomNum() {
         return Math.floor(Math.random() * 1000);
     }
 
-    render() {      
-        const { track } = this.props; 
-      
-        return ( 
-            <Link className="track-index-item" to={`/tracks/${track.id}`}>        
-                <p className="track-index-item-id">{track.id}</p>
-                <div className="track-index-item-image" style={{ background: `url(${track.artwork_path}`, backgroundPosition: "center", backgroundSize: "cover" }}> </div>
-                <div className="track-index-item-title-main">
-                    <p className="track-index-item-title">{track.title}</p>
-                    <p className="track-index-item-lyrics">LYRICS</p>
-                </div>
-                <p className="track-index-item-artist">{track.artist}</p>
-                <div className="track-index-item-fire-main">
-                    <img className="track-index-item-fire" src={window.fireIcon} />
-                    <p className="track-index-item-fire-number">{this.randomNum()}</p>
-                </div>
-                <div className="track-index-item-eye-main">
-                    <img className="track-index-item-eye" src={window.eyeIcon} />
-                    <p className="track-index-item-eye-number">{this.randomNum() * 10}</p>
-                </div>
-            </Link>
-        );
-    }
-};
+    return ( 
+        <Link className="track-index-item" to={`/tracks/${track.id}`}>        
+            <p className="track-index-item-id">{track.id}</p>
+            <div 
+                className="track-index-item-image" 
+                style={{ 
+                    background: `url(${track.artwork_path}`, 
+                    backgroundPosition: "center", 
+                    backgroundSize: "cover" }
+                }
+            > 
+            </div>
+            <div className="track-index-item-title-main">
+                <p className="track-index-item-title">{track.title}</p>
+                <p className="track-index-item-lyrics">LYRICS</p>
+            </div>
+            <p className="track-index-item-artist">{track.artist}</p>
+            <div className="track-index-item-fire-main">
+                <img className="track-index-item-fire" src={window.fireIcon} />
+                <p className="track-index-item-fire-number">{randomNum()}</p>
+            </div>
+            <div className="track-index-item-eye-main">
+                <img className="track-index-item-eye" src={window.eyeIcon} />
+                <p className="track-index-item-eye-number">{randomNum() * 10}</p>
+            </div>
+        </Link>
+    )
+}
 
 export default TrackIndexItem;
