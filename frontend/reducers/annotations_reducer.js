625651bc5327950bf74ef382bb40f9a988cfbae3
thedaebu <min.kim163@gmail.com>
2021-11-29 16:57:39 -0500

    change  reducer to remove all annotations from state when RECEIVE_TRACKS action is received

--- frontend/reducers/annotations_reducer.js
+++ frontend/reducers/annotations_reducer.js
@@ -1,5 +1,5 @@
 import { RECEIVE_ANNOTATION } from "../actions/annotation_actions";
-import { RECEIVE_TRACK } from "../actions/track_actions";
+import { RECEIVE_TRACK, RECEIVE_TRACKS } from "../actions/track_actions";
 
 const annotationsReducer = (state = {}, action) => {
     Object.freeze(state);
@@ -7,6 +7,8 @@ const annotationsReducer = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_TRACK:       
             return Object.assign({}, action.annotations);
+        case RECEIVE_TRACKS:
+            return Object.assign({});
         case RECEIVE_ANNOTATION:   
             return Object.assign({}, state, {[action.annotation.id]: action.annotation});
         default:
