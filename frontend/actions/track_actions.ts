208cbc4508197ed3fb85668a4a5143b3a1a912fd
thedaebu <min.kim163@gmail.com>
2021-11-29 15:20:57 -0500

    change trackID type from number to string

--- frontend/actions/track_actions.ts
+++ frontend/actions/track_actions.ts
@@ -63,7 +63,7 @@ export const fetchTracks = () => (dispatch: Dispatch<AnyAction>) => {
     );
 };
 
-export const fetchTrack = (trackId: number) => (dispatch: Dispatch<AnyAction>) => {
+export const fetchTrack = (trackId: string) => (dispatch: Dispatch<AnyAction>) => {
     return (
         TrackApiUtil.fetchTrack(trackId).then((track: ReceivedTrack) => dispatch(receiveTrack(track)))
     );
