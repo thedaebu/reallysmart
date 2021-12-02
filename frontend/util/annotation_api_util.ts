747979884b29ca07c2cfcd12768b335d3bb87f29
thedaebu <min.kim163@gmail.com>
2021-12-01 21:31:42 -0500

    reformat for typescript

--- frontend/util/annotation_api_util.ts
+++ frontend/util/annotation_api_util.ts
@@ -1,4 +1,12 @@
-export const fetchAnnotation = (annotationId) => {
+type NewAnnotation = {
+    body: string,
+    annotator_id: number,
+    track_id: number,
+    start_index: number,
+    end_index: number
+}
+
+export const fetchAnnotation = (annotationId: string) => {
     return (
         $.ajax({
             method: "GET",
@@ -7,7 +15,7 @@ export const fetchAnnotation = (annotationId) => {
     );
 };
 
-export const createAnnotation = (annotation) => {
+export const createAnnotation = (annotation: NewAnnotation) => {
     return (
         $.ajax({
             method: "POST",
