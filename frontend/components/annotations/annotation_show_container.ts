35b0b7128b04049749fc9db500d48d96b0d68a2b
thedaebu <min.kim163@gmail.com>
2021-12-06 15:18:57 -0500

    reformat for typescript

---
+++ frontend/components/annotations/annotation_show_container.ts
@@ -0,0 +1,51 @@
+import { connect } from "react-redux";
+import { createAnnotation, fetchAnnotation } from "../../actions/annotation_actions";
+import { closeAnnotationModal } from "../../actions/annotation_modal_actions";
+import { fetchTrack } from "../../actions/track_actions";
+import AnnotationShow from "./annotation_show";
+
+type State = {
+    entities: Entities,
+    modal: Modal
+}
+interface Entities {
+    annotations: AnnotationKey,
+}
+interface AnnotationKey {
+    [key: number]: Annotation
+}
+interface Annotation {
+    annotator: string,
+    annotator_id: number,
+    body: string,
+    comment_ids: Array<number>,
+    end_index: number,
+    id: number,
+    start_index: number,
+    track_id: number,
+    votes: number
+}
+interface Modal {
+    annotationModal: boolean
+}
+type ownProps = {
+    annotationId: number
+}
+
+const mSTP = (state: State, ownProps: ownProps) => {
+    return ({
+        annotation: state.entities.annotations[ownProps.annotationId],
+        annotationModal: state.modal.annotationModal
+    });
+};
+
+const mDTP = (dispatch: Function) => {
+    return ({
+        createAnnotation: (annotation: Annotation) => dispatch(createAnnotation(annotation)),
+        fetchAnnotation: (annotationId: number) => dispatch(fetchAnnotation(annotationId)),
+        fetchTrack: (trackId: string) => dispatch(fetchTrack(trackId)),
+        closeAnnotationModal: () => dispatch(closeAnnotationModal())
+    });
+};
+
+export default connect(mSTP, mDTP)(AnnotationShow);
