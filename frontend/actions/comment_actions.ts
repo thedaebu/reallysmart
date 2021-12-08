653a791841d8a5bd9c8f781a6cd8bfaa33112b02
thedaebu <min.kim163@gmail.com>
2021-12-07 21:22:21 -0500

    reformat for typescript

---
+++ frontend/actions/comment_actions.ts
@@ -0,0 +1,35 @@
+import { Dispatch } from "react";
+import { AnyAction } from "redux";
+import * as CommentApiUtil from "./../util/comment_api_util";
+
+export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
+
+interface Comment {
+    body: string,
+    commentable_id: number,
+    commenter: string,
+    commenter_id: number,
+    id: number,
+    updated_at: string,
+    votes: number
+}
+
+const receiveComment = (comment: Comment) => {
+    debugger
+    return ({
+        type: RECEIVE_COMMENT,
+        comment
+    });
+};
+
+export const fetchComment = (commentId: number) => (dispatch: Dispatch<AnyAction>) => {
+    return (
+        CommentApiUtil.fetchComment(commentId).then((comment: Comment) => dispatch(receiveComment(comment)))
+    );
+};
+
+export const createComment = (comment: Comment) => (dispatch: Dispatch<AnyAction>) => {
+    return (
+        CommentApiUtil.createComment(comment).then((comment: Comment) => dispatch(receiveComment(comment)))
+    );
+};
