a1818e3edbf7a06acb29aec2d4c4efe0d3a2400b
thedaebu <min.kim163@gmail.com>
2021-12-13 15:51:29 -0500

    change clearSearchField function implementation into new seprate function

--- frontend/components/header/searchbar_items.tsx
+++ frontend/components/header/searchbar_items.tsx
@@ -1,13 +1,34 @@
-import React, { useEffect } from "react";
+import React, { MouseEvent, useEffect } from "react";
 import SearchbarItem from "../header/searchbar_item";
 
-function SearchbarItems(props) {
-    const { searches, searchField, clearSearchField } = props;
-    const { siteLocation } = props.match.params;
+type Props = {
+    clearSearchField: Function,
+    searches: Array<Track>
+    searchField: string,
+    siteLocation: string,
+}
+interface Track {
+    annotation_ids: Array<number>,
+    artist: string,
+    artwork_path: string,
+    comment_ids: Array<number>,
+    id: number,
+    lyrics: string,
+    title: string
+}
+
+function SearchbarItems(props: Props) {
+    const { searches, searchField, clearSearchField, siteLocation } = props;
 
     useEffect(() => {
         clearSearchField();
     }, [siteLocation])
+
+    function clearSearch(e: MouseEvent<HTMLUListElement>) {
+        e.preventDefault();
+
+        clearSearchField();
+    }
 
     function searchbarResults() {
         if (searches.length > 0 && searchField !== "") {
@@ -15,7 +36,7 @@ function SearchbarItems(props) {
                 <div className="searchbar-items-modal">
                     <p className="searchbar-search-results">SEARCH RESULTS</p>
                     <p className="searchbar-search-songs">SONGS</p>
-                    <ul onClick={clearSearchField}>
+                    <ul onClick={clearSearch}>
                         {searchbarItems()}
                     </ul>
                 </div>
