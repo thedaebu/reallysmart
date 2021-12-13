c4bd45d5f0f11b5c2c03a22813ab708c7f1b4e66
thedaebu <min.kim163@gmail.com>
2021-12-13 14:58:18 -0500

    reformat for typescript

---
+++ frontend/components/header/searchbar_items_container.ts
@@ -0,0 +1,38 @@
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import SearchbarItems from "./searchbar_items";
+
+type State = {
+    entities: Entities
+}
+interface Entities {
+    searches: Searches
+}
+type Searches = {
+    [key: number]: Track
+}
+interface Track {
+    annotation_ids: Array<number>,
+    artist: string,
+    artwork_path: string,
+    comment_ids: Array<number>,
+    id: number,
+    lyrics: string,
+    title: string
+}
+type OwnProps = {
+    location: OwnPropsLocation
+}
+interface OwnPropsLocation {
+    pathname: string
+}
+
+const mSTP = (state: State, ownProps: OwnProps) => {
+    debugger
+    return ({
+        searches: Object.values(state.entities.searches),
+        siteLocation: ownProps.location.pathname
+    });
+};
+
+export default withRouter(connect(mSTP, null)(SearchbarItems));
