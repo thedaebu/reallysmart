fc762ce648598cf839ea184af834e8f2e3aa0cf4
thedaebu <min.kim163@gmail.com>
2021-12-13 12:22:18 -0500

    reformat for typescript

---
+++ frontend/actions/search_actions.ts
@@ -0,0 +1,39 @@
+import { Dispatch } from "react";
+import { AnyAction } from "redux";
+import * as SearchApiUtil from "./../util/search_api_util";
+
+export const RECEIVE_SEARCHES = "RECEIVE_SEARCHES";
+export const CLEAR_SEARCHES = "CLEAR_SEARCHES";
+
+type Searches = {
+    [key: number]: Track
+}
+interface Track {
+    annotation_ids: Array<number>,
+    artist: string,
+    artwork_path: string,
+    comment_ids: Array<number>,
+    id: number,
+    lyrics: string,
+    title: string
+}
+
+const receiveSearches = (searches: Searches) => {
+    return ({
+        type: RECEIVE_SEARCHES,
+        searches
+    });
+};
+
+export const clearSearches = () => {
+    return ({
+        type: CLEAR_SEARCHES
+    });
+};
+
+export const fetchSearches = (search: string) => (dispatch: Dispatch<AnyAction>) => {
+    debugger
+    return (
+        SearchApiUtil.fetchSearches(search).then(searches => dispatch(receiveSearches(searches)))
+    );
+};
