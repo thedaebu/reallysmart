a1a728869a9ab02e2717fb39f611304425f43157
thedaebu <min.kim163@gmail.com>
2021-12-20 19:34:10 -0500

    fix grammar errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1195,7 +1195,7 @@ river_vote8 = Vote.create(
 )
 
 reflection_annotation1 = Annotation.create(
-    body: "Niki is allowing Selene to take over she and Selene want the same thing.",
+    body: "She wants to know why she does not see her true self.",
     annotator_id: reallysmart.id,
     track_id: reflection.id,
     start_index: 344,
