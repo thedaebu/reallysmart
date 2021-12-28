d34ff7943f417ee2c7659754c16e8e636938c047
thedaebu <min.kim163@gmail.com>
2021-12-28 14:21:31 -0500

    remove fmSTP keys for currentVote and currentVoteStatus and remove associated keys from OwnProps type

--- frontend/components/votes/votes_show_container.ts
+++ frontend/components/votes/votes_show_container.ts
@@ -1,47 +1,18 @@
 import { connect } from "react-redux";
-import { Annotation, Comment, CreatedVote, State, User, Vote } from "../../my_types";
+import { CreatedVote, State } from "../../my_types";
 import { fetchAnnotation } from "../../actions/annotation_actions";
 import { fetchComment } from "../../actions/comment_actions";
 import { createVote, deleteVote, fetchVote } from "../../actions/vote_actions";
 import VotesShow from "./votes_show";
 
 type OwnProps = {
-    numberOfVotes: number,
-    parent: Annotation | Comment,
-    voteableId: number,
     voteableType: string
 }
 
-const mSTP = (state: State, ownProps: OwnProps) => {
-    const currentUser: User = state.entities.user[state.session.id];
-    const currentUserVotes: Array<Vote> = currentUser && Object.keys(state.entities.votes).length > 0 
-        ? currentUser.vote_ids.map((id: number) => {
-            if (state.entities.votes[id]) {
-                return state.entities.votes[id];
-            }
-        }).filter((vote: Vote | undefined) => {
-            if (vote !== undefined) {
-                return vote;
-            }
-        })
-        : Array()
-    function findCurrentVote(votes: Array<Vote>) {
-        for (let vote of votes) {
-            if (vote.voteable_type === ownProps.voteableType && vote.voteable_id === ownProps.voteableId) {
-                return vote;
-            }
-        }
-        return null;
-    }
-    const currentVote = findCurrentVote(currentUserVotes);
-    const currentVoteStatus: boolean = currentVote !== null 
-        ? true 
-        : false
-
+const mSTP = (state: State) => {
     return ({
-        currentUser: currentUser,
-        currentVote: currentVote,
-        currentVoteStatus: currentVoteStatus
+        currentUser: state.entities.user[state.session.id],
+        votes: state.entities.votes
     });
 };
 
