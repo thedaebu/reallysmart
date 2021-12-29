8c27868ea07fba2c625224fa45bc5b40f772fc4f
thedaebu <min.kim163@gmail.com>
2021-12-29 14:24:19 -0500

    refactor so annotation comments are fetched when annotation show component is rendered for existing annotation

--- frontend/components/annotations/annotation_show_container.ts
+++ frontend/components/annotations/annotation_show_container.ts
@@ -4,6 +4,7 @@ import { createAnnotation } from "../../actions/annotation_actions";
 import { closeAnnotationModal } from "../../actions/annotation_modal_actions";
 import { fetchTrack } from "../../actions/track_actions";
 import AnnotationShow from "./annotation_show";
+import { fetchComment } from "../../actions/comment_actions";
 
 type OwnProps = {
     annotationId: number
@@ -23,6 +24,7 @@ const mDTP = (dispatch: Function) => {
     return ({
         createAnnotation: (annotation: CreatedAnnotation) => dispatch(createAnnotation(annotation)),
         closeAnnotationModal: () => dispatch(closeAnnotationModal()),
+        fetchComment: (commentId: number) => dispatch(fetchComment(commentId)),
         fetchTrack: (trackId: string) => dispatch(fetchTrack(trackId))
     });
 };
