09f40f05b4c3af6f7ff3260f48c83480dfe20ede
thedaebu <min.kim163@gmail.com>
2022-01-14 13:21:24 -0500

    uupdate reducer for remove case

--- frontend/reducers/annotations_reducer.ts
+++ frontend/reducers/annotations_reducer.ts
@@ -1,15 +1,19 @@
-import { RECEIVE_ANNOTATION } from "../actions/annotation_actions";
+import { RECEIVE_ANNOTATION, REMOVE_ANNOTATION } from "../actions/annotation_actions";
 import { RECEIVE_TRACKS } from "../actions/track_actions";
 import { Action, Annotation } from "../my_types";
 
 const annotationsReducer = (state: {[key: number]: Annotation} = {}, action: Action) => {
     Object.freeze(state);
+    const newState: {[key: number]: Annotation} = Object.assign({}, state);
 
     switch (action.type) {
         case RECEIVE_TRACKS:
             return Object.assign({});
         case RECEIVE_ANNOTATION:
             return Object.assign({}, state, {[action.annotation.id]: action.annotation});
+        case REMOVE_ANNOTATION:
+            delete newState[action.annotationId];
+            return newState;
         default:
             return state;
     }
--- frontend/reducers/comments_reducer.ts
+++ frontend/reducers/comments_reducer.ts
@@ -1,15 +1,19 @@
 import { RECEIVE_TRACKS } from "../actions/track_actions"
-import { RECEIVE_COMMENT } from "../actions/comment_actions";
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from "../actions/comment_actions";
 import { Action, Comment } from "../my_types";
 
 const commentsReducer = (state: {[key: number]: Comment} = {}, action: Action) => {
     Object.freeze(state);
+    const newState: {[key: number]: Comment} = Object.assign({}, state);
 
     switch (action.type) {
         case RECEIVE_TRACKS:
             return Object.assign({});
         case RECEIVE_COMMENT:
             return Object.assign({}, state, {[action.comment.id]: action.comment});
+        case REMOVE_COMMENT:
+            delete newState[action.commentId];
+            return newState;
         default:
             return state;
     }
