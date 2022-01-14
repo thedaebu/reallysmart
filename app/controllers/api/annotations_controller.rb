6d266a8168bbb5eb2256854da5bb702d9e28fc90
thedaebu <min.kim163@gmail.com>
2022-01-14 12:11:20 -0500

    update controller for update and destroy routes

--- app/controllers/api/annotations_controller.rb
+++ app/controllers/api/annotations_controller.rb
@@ -1,15 +1,33 @@
 class Api::AnnotationsController < ApplicationController
     def show
-        @annotation = Annotation.find_by(id: params[:id])
+        @annotation = Annotation.find(params[:id])
         render :show
     end
 
     def create
-        @annotation = Annotation.create(annotation_params)
+        @annotation = Annotation.new(annotation_params)
         if @annotation.save
             render :show
         else
             render json: @annotation.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @annotation = Annotation.find(params[:id])
+        if @annotation.update(annotation_params)
+            render :show
+        else
+            render json: @annotation.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @annotation = Annotation.find(params[:id])
+        if @annotation
+            @annotation.destroy
+        else
+            render json: ['The annotation does not exist.']
         end
     end
 
--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -13,6 +13,24 @@ class Api::CommentsController < ApplicationController
         end
     end
 
+    def update
+        @comment = Comment.find(params[:id])
+        if @comment.update(comment_params)
+            render :show
+        else
+            render json: @comment.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        if @comment
+            @comment.destroy
+        else
+            render json: ['The comment does not exist']
+        end
+    end
+    
     private
     def comment_params
         params.require(:comment).permit(:body, :commenter_id, :commentable_type, :commentable_id)
