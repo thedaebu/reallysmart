8572c582c7afdbf6f181bd9e5ebb51d86ceaf1d9
thedaebu <min.kim163@gmail.com>
2022-03-01 20:50:02 -0500

    add queries for associated annotations, comments, and votes from view to controller

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -6,6 +6,15 @@ class Api::TracksController < ApplicationController
 
     def show
         @track = Track.find(params[:id])
+        @annotations = @track.annotations
+        comments = @track.comments
+        annotation_comments = @track.annotation_comments
+        annotation_votes = @track.annotation_votes
+        annotation_comment_votes = @track.annotation_comment_votes
+        comment_votes = @track.comment_votes
+        @comments = comments + annotation_comments
+        @votes = annotation_votes + annotation_comment_votes + comment_votes
+
         render :show
     end
 end
