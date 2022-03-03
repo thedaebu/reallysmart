d5143539a265ff77c632dc21ecc5ba9079ba71c7
thedaebu <min.kim163@gmail.com>
2022-03-02 20:46:09 -0500

    refactor to make more concise

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -7,13 +7,8 @@ class Api::TracksController < ApplicationController
     def show
         @track = Track.find(params[:id])
         @annotations = @track.annotations
-        comments = @track.comments
-        annotation_comments = @track.annotation_comments
-        annotation_votes = @track.annotation_votes
-        annotation_comment_votes = @track.annotation_comment_votes
-        comment_votes = @track.comment_votes
-        @comments = comments + annotation_comments
-        @votes = annotation_votes + annotation_comment_votes + comment_votes
+        @comments = @track.comments + @track.annotation_comments
+        @votes = @track.annotation_votes + @track.annotation_comment_votes + @track.comment_votes
         render :show
     end
 end
