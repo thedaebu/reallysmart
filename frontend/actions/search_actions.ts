417ed2d402885f1ee4a5c35626aa536cd6c8606c
thedaebu <min.kim163@gmail.com>
2022-03-09 18:02:50 -0500

    add type for receivedSearches

--- frontend/actions/search_actions.ts
+++ frontend/actions/search_actions.ts
@@ -1,22 +1,22 @@
 import { Dispatch } from "react";
 import { AnyAction } from "redux";
-import { Track } from "../my_types";
+import { ReceivedSearches } from "../my_types";
 import * as SearchApiUtil from "./../util/search_api_util";
 
 export const RECEIVE_SEARCHES = "RECEIVE_SEARCHES";
 export const CLEAR_SEARCHES = "CLEAR_SEARCHES";
 
-const receiveSearches = (searches: Array<Track>) => {
+const receiveSearches = (receivedSearches: ReceivedSearches) => {
     return ({
         type: RECEIVE_SEARCHES,
-        searches
+        searches: receivedSearches.searches
     });
 };
 
 export const fetchSearches = (search: string) => (dispatch: Dispatch<AnyAction>) => {
     return (
         SearchApiUtil.fetchSearches(search)
-            .then((searches: Array<Track>) => dispatch(receiveSearches(searches)))
+            .then((receivedSearches: ReceivedSearches) => dispatch(receiveSearches(receivedSearches)))
     );
 };
 export const clearSearches = () => {
