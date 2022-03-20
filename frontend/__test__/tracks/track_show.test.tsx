19e6fc77eee1ebca1610c0cf3fc436479665e4fd
thedaebu <min.kim163@gmail.com>
2022-03-20 17:08:31 -0400

    finish track show test

--- frontend/__test__/tracks/track_show.test.tsx
+++ frontend/__test__/tracks/track_show.test.tsx
@@ -9,29 +9,48 @@ import thunk from 'redux-thunk';
 import server from "../msw_server"
 import TrackShow from "../../components/tracks/track_show";
 import * as trackActions from "../../actions/track_actions";
+import { testMatch, testTrackStore } from "../test_store_data";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const testStore = mockStore(testTrackStore);
+
+const useFetchTrack = jest.spyOn(trackActions, 'fetchTrack');
+const useMockDispatch = jest.spyOn(reactRedux, 'useDispatch');
+const useMockEffect = jest.spyOn(React, 'useEffect');
 
 describe("track show", () => {
-    const middlewares = [thunk];
-    const mockStore = configureMockStore(middlewares);
-    const testStore = mockStore({ entities: {tracks:  }});
-
-    const useFetchTracks = jest.spyOn(trackActions, 'fetchTracks');
-    const useMockEffect = jest.spyOn(React, 'useEffect');
-    const useMockDispatch = jest.spyOn(reactRedux, 'useDispatch');
-
-    beforeAll(() => server.listen());
-    beforeEach(() => {
-        render(
-            <BrowserRouter>
-                <Provider store={testStore}>
-                    <TrackShow history={undefined} location={undefined} match={undefined} />
-                </Provider>
-            </BrowserRouter>
-        )
-    });
+    // beforeAll(() => server.listen());
     afterEach(() => {
         cleanup()
-        server.resetHandlers()
+        // server.resetHandlers()
     });
-    afterAll(() => server.close());
+    // afterAll(() => server.close());
+
+    render(
+        <BrowserRouter>
+            <Provider store={testStore}>
+                <TrackShow history={undefined} location={undefined} match={testMatch} />
+            </Provider>
+        </BrowserRouter>
+    )
+    describe("useEffect", () => {
+        test("useEffect should be called", () => {
+            expect(useMockEffect).toHaveBeenCalled();
+        });
+        test("dispatch should be called", () => {
+            expect(useMockDispatch).toHaveBeenCalled();
+        });
+        test("fetchTrack should be called", () => {
+            expect(useFetchTrack).toHaveBeenCalled();
+        });
+    });
+    describe("track show header", () => {
+        const track = testTrackStore.entities.tracks[1]
+        const header = screen.getByTestId("track-show-header");
+        test("contains the artist and title of the track", () => {
+            expect(header).toHaveTextContent(track.artist);
+            expect(header).toHaveTextContent(track.title);
+        });
+    });
 })
