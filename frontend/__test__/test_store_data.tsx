354d842ee58f52f3dc30a9ab9f0f8bc32eee7c62
thedaebu <min.kim163@gmail.com>
2022-03-22 20:12:09 -0400

    refactor data

--- frontend/__test__/test_store_data.tsx
+++ frontend/__test__/test_store_data.tsx
@@ -1,3 +1,41 @@
+const tracks = {
+    1:  {
+        artist: "NIKI",
+        artwork_path: "https://i.ytimg.com/vi/GBqqoPSJ9GY/maxresdefault.jpg",
+        id: 1,
+        title: "Selene"
+    },
+    2:  {
+        artist: "Modjo",
+        artwork_path: "https://i.ytimg.com/vi/Z0V4CtdXlhk/maxresdefault.jpg",
+        id: 2,
+        title: "Lady"
+    },
+    3:  {
+        artist: "Drake",
+        artwork_path: "https://images.genius.com/65dacc63f81321a1cee1435f303a1bf5.1000x1000x1.jpg",
+        id: 3,
+        title: "Fake Love"
+    },
+    4:  {
+        artist: "Bishop Briggs",
+        artwork_path: "https://images.genius.com/2f7cccb4dfe4cd619758a9d436faa5eb.1000x1000x1.png",
+        id: 4,
+        title: "River"
+    },
+    5:  {
+        artist: "Lea Salonga",
+        artwork_path: "https://i.ytimg.com/vi/RxUmbraYDcE/hqdefault.jpg",
+        id: 5,
+        title: "Reflection"
+    },
+    6:  {
+        artist: "Ne-Yo",
+        artwork_path: "https://images-na.ssl-images-amazon.com/images/I/516J-AHuqOL._SY355_.jpg",
+        id: 6,
+        title: "Stay"
+    }
+};
 export const testMatch = {
     isExact: true,
     params: {
@@ -6,52 +44,9 @@ export const testMatch = {
     path: "/tracks/:trackId",
     url: "/tracks/1"
 };
-export const testLocation = {
-    hash: "",
-    pathname: "/tracks/10",
-    search: "",
-    state: true
-};
 export const testIndexStore = {
     entities: {
-        tracks: {
-            1:  {
-                artist: "NIKI",
-                artwork_path: "https://i.ytimg.com/vi/GBqqoPSJ9GY/maxresdefault.jpg",
-                id: 1,
-                title: "Selene"
-            },
-            2:  {
-                artist: "Modjo",
-                artwork_path: "https://i.ytimg.com/vi/Z0V4CtdXlhk/maxresdefault.jpg",
-                id: 2,
-                title: "Lady"
-            },
-            3:  {
-                artist: "Drake",
-                artwork_path: "https://images.genius.com/65dacc63f81321a1cee1435f303a1bf5.1000x1000x1.jpg",
-                id: 3,
-                title: "Fake Love"
-            },
-            4:  {
-                artist: "Bishop Briggs",
-                artwork_path: "https://images.genius.com/2f7cccb4dfe4cd619758a9d436faa5eb.1000x1000x1.png",
-                id: 4,
-                title: "River"
-            },
-            5:  {
-                artist: "Lea Salonga",
-                artwork_path: "https://i.ytimg.com/vi/RxUmbraYDcE/hqdefault.jpg",
-                id: 5,
-                title: "Reflection"
-            },
-            6:  {
-                artist: "Ne-Yo",
-                artwork_path: "https://images-na.ssl-images-amazon.com/images/I/516J-AHuqOL._SY355_.jpg",
-                id: 6,
-                title: "Stay"
-            }
-        }
+        tracks: tracks
     }
 };
 export const testTrackStore = {
@@ -105,44 +100,7 @@ export const testTrackStore = {
                 updated_at: "2022-03-20T17:56:15.629Z"
             },
         },
-        searches: {
-            1:  {
-                artist: "NIKI",
-                artwork_path: "https://i.ytimg.com/vi/GBqqoPSJ9GY/maxresdefault.jpg",
-                id: 1,
-                title: "Selene"
-            },
-            2:  {
-                artist: "Modjo",
-                artwork_path: "https://i.ytimg.com/vi/Z0V4CtdXlhk/maxresdefault.jpg",
-                id: 2,
-                title: "Lady"
-            },
-            3:  {
-                artist: "Drake",
-                artwork_path: "https://images.genius.com/65dacc63f81321a1cee1435f303a1bf5.1000x1000x1.jpg",
-                id: 3,
-                title: "Fake Love"
-            },
-            4:  {
-                artist: "Bishop Briggs",
-                artwork_path: "https://images.genius.com/2f7cccb4dfe4cd619758a9d436faa5eb.1000x1000x1.png",
-                id: 4,
-                title: "River"
-            },
-            5:  {
-                artist: "Lea Salonga",
-                artwork_path: "https://i.ytimg.com/vi/RxUmbraYDcE/hqdefault.jpg",
-                id: 5,
-                title: "Reflection"
-            },
-            6:  {
-                artist: "Ne-Yo",
-                artwork_path: "https://images-na.ssl-images-amazon.com/images/I/516J-AHuqOL._SY355_.jpg",
-                id: 6,
-                title: "Stay"
-            }
-        },
+        searches: tracks,
         tracks: {
             1: {
                 artist: "NIKI",
