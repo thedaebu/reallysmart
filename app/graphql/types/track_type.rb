c1d1bb373f7b1640117c4244239b9fd4261321f6
thedaebu <min.kim163@gmail.com>
2022-03-25 20:48:48 -0400

    add track_comments and track_votes methods

--- app/graphql/types/track_type.rb
+++ app/graphql/types/track_type.rb
@@ -2,18 +2,22 @@
 
 module Types
   class TrackType < Types::BaseObject
-    field :id, ID, null: false
     field :artist, String, null: false
     field :artwork_path, String, null: false
+    field :id, ID, null: false
     field :lyrics, String, null: false
     field :title, String, null: false
-    field :annotations, [Types::AnnotationType], null: false
-    field :annotation_votes, [Types::VoteType], null: false
-    field :annotation_comments, [Types::CommentType], null: false
-    field :annotation_comment_votes, [Types::VoteType], null: false
-    field :comments, [Types::CommentType], null: false
-    field :comment_votes, [Types::VoteType], null: false
+    field :track_comments, [Types::CommentType], null: false
+    field :track_votes, [Types::VoteType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def track_comments
+      object.comments + object.annotation_comments
+    end
+
+    def track_votes
+      object.annotation_votes + object.annotation_comment_votes + object.comment_votes
+    end
   end
 end
