55c258f45333c7e07f7948712f52fa940067ffd1
thedaebu <min.kim163@gmail.com>
2022-03-27 12:40:03 -0400

    finish connection between frontend and backend for graphql

--- frontend/components/root.tsx
+++ frontend/components/root.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import { AnyAction, Store } from "redux";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  HttpLink
+} from "@apollo/client";
 import App from "./app";
 
 type Props = {
@@ -10,13 +18,31 @@ type Props = {
 
 function Root(props: Props) {
     const { store } = props;
+    let uri: string
+
+    if (process.env.NODE_ENV == "production") {
+        uri = "https://really-smart.herokuapp.com";
+    } else {
+        uri = "http://localhost:3000/graphql";
+    }
+    const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('content');
+    const client = new ApolloClient({
+        uri: uri,
+        credentials: 'same-origin',
+        headers: {
+            'X-CSRF-Token': csrfToken
+        },
+        cache: new InMemoryCache()
+    });
 
     return (
-        <Provider store={ store }>
-            <HashRouter>
-                <App />
-            </HashRouter>
-        </Provider>
+        <ApolloProvider client={client}>
+            <Provider store={ store }>
+                <HashRouter>
+                    <App />
+                </HashRouter>
+            </Provider>
+        </ApolloProvider>
     );
 }
 
