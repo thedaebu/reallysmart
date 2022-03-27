1e68958ef60781bb7896269fe05b36d3ab201f7d
thedaebu <min.kim163@gmail.com>
2022-03-27 14:20:50 -0400

    add graphql_client file

---
+++ frontend/graphql_client/graphql_client.ts
@@ -0,0 +1,21 @@
+import {
+  ApolloClient,
+  InMemoryCache
+} from "@apollo/client";
+
+let uri: string
+
+if (process.env.NODE_ENV == "production") {
+    uri = "https://really-smart.herokuapp.com";
+} else {
+    uri = "http://localhost:3000/graphql/";
+}
+const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('content');
+export const graphqlClient = new ApolloClient({
+    cache: new InMemoryCache(),
+    credentials: 'same-origin',
+    headers: {
+        'X-CSRF-Token': csrfToken
+    },
+    uri: uri
+});
