f51645d523a8ac78a0e3055c3f6d2a3c22e8c9f9
thedaebu <min.kim163@gmail.com>
2022-03-27 09:52:57 -0400

    change track_comments/votes to total_comments/votes

--- app/graphql/types/track_type.rb
+++ app/graphql/types/track_type.rb
@@ -7,16 +7,16 @@ module Types
     field :id, ID, null: false
     field :lyrics, String, null: false
     field :title, String, null: false
-    field :track_comments, [Types::CommentType], null: false
-    field :track_votes, [Types::VoteType], null: false
+    field :total_comments, [Types::CommentType], null: false
+    field :total_votes, [Types::VoteType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    def track_comments
+    def total_comments
       object.comments + object.annotation_comments
     end
 
-    def track_votes
+    def total_votes
       object.annotation_votes + object.annotation_comment_votes + object.comment_votes
     end
   end
