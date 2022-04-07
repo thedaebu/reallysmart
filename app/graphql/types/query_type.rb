540228199108439bf8e67f62009b732a7f652f62
thedaebu <min.kim163@gmail.com>
2022-04-07 13:44:56 -0400

    create graphql query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,49 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :annotation, Types::AnnotationType, null: false do
+      argument :id, ID, required: true
+    end
+    field :comment, Types::CommentType, null: false do
+      argument :id, ID, required: true
+    end
+    field :searches, [Types::TrackType], null: false do
+      argument :search, String, required: true
+    end
+    field :track, Types::TrackType, null: false do
+      argument :id, ID, required: true
+    end
+    field :tracks, [Types::TrackType], null: false
+    field :user, Types::UserType, null: false do 
+      argument :id, ID, required: true
+    end
+    field :vote, Types::VoteType, null: false do
+      argument :id, ID, required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def annotation(id:)
+      Annotation.find(id)
+    end
+    def comment(id:)
+      Comment.find(id)
+    end
+    def searches(search:)
+      tags = Tag.where("lower(name) LIKE ?", "%#{search.downcase}%")
+      result = {}
+      tags.each{|tag| result[tag.track.id] = tag.track}
+      result.values
+    end
+    def track(id:)
+      Track.find(id)
+    end
+    def tracks
+      Track.all
+    end
+    def user(id:)
+      User.find(id)
+    end
+    def vote(id:)
+      Vote.find(id)
     end
   end
 end
