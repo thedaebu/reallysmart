fa91dc01cd3075fbb431055d37a76a217963b410
thedaebu <min.kim163@gmail.com>
2022-04-16 11:13:05 -0400

    change value for top style attribute to fix test error log

--- frontend/components/annotations/annotation_show.tsx
+++ frontend/components/annotations/annotation_show.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Dispatch, MouseEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, Dispatch, MouseEvent, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import * as AnnotationActions from "../../actions/annotation_actions";
