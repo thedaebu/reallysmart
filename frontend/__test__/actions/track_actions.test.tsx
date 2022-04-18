e41c08c63f99ec4e4609383d57ddbeebd4afee03
thedaebu <min.kim163@gmail.com>
2022-04-18 15:43:03 -0400

    add track actions tests

--- frontend/__test__/actions/track_actions.test.tsx
+++ frontend/__test__/actions/track_actions.test.tsx
@@ -16,37 +16,41 @@ describe("track actions", () => {
             expect(TrackActions.RECEIVE_TRACK).toEqual("RECEIVE_TRACK");
         });
     });
-    // describe("functions", () => {
-    //     let store: any;
-    //     beforeEach(() => {
-    //         store = mockStore({ tracks: {} });
-    //     });
-    //     describe("fetchTracks", () => {
-    //         test("is exported", () => {
-    //             expect(typeof TrackAPIUtil.fetchTracks).toEqual("function");
-    //         });
-    //         test("dispatches RECEIVE_TRACKS when fetchTracks is called", () => {
-    //             const tracks = testTracks;
-    //             TrackAPIUtil.fetchTracks = jest.fn(() => (
-    //                 Promise.resolve(tracks)
-    //             ));
-    //             const actions: any = [{type: "RECEIVE_TRACKS", tracks}];
-    //             return store.dispatch(TrackActions.fetchTracks()).then(() => {
-    //                 expect(store.getActions()).toEqual(actions);
-    //             })
-    //         });
-    //     });
-        // describe("fetchTrack", () => {
-        //     test("dispatches RECEIVE_TRACK when fetchTrack is called", () => {
-        //         const track = testTrackShowStore.entities;
-        //         TrackAPIUtil.fetchTrack = jest.fn((trackId) => (
-        //             Promise.resolve(track)
-        //         ));
-        //         const actions: any = [{type: "RECEIVE_TRACK", track: track}];
-        //         return store.dispatch(TrackActions.fetchTrack("1")).then(() => {
-        //             expect(store.getActions()).toEqual(actions);
-        //         })
-        //     });
-        // });
-    // });
+    describe("functions", () => {
+        let store: any;
+        beforeEach(() => {
+            store = mockStore({tracks: {}});
+        });
+        afterEach(() => {
+            store.clearActions();
+        })
+        describe("fetchTracks", () => {
+            test("is exported", () => {
+                expect(typeof TrackAPIUtil.fetchTracks).toEqual("function");
+            });
+            test("dispatches RECEIVE_TRACKS when fetchTracks is called", () => {
+                TrackAPIUtil.fetchTracks = jest.fn(() => (
+                    Promise.resolve({})
+                ));
+                const actions: any = [{type: "RECEIVE_TRACKS"}];
+                return store.dispatch(TrackActions.fetchTracks()).then(() => {
+                    expect(store.getActions()).toEqual(actions);
+                });
+            });
+        });
+        describe("fetchTrack", () => {
+            test("is exported", () => {
+                expect(typeof TrackAPIUtil.fetchTrack).toEqual("function");
+            });
+            test("dispatches RECEIVE_TRACK when fetchTrack is called", () => {
+                TrackAPIUtil.fetchTrack = jest.fn(() => (
+                    Promise.resolve({})
+                ));
+                const actions: Array<{[type: string]: string}> = [{type: "RECEIVE_TRACK"}];
+                return store.dispatch(TrackActions.fetchTrack("1")).then(() => {
+                    expect(store.getActions()).toEqual(actions);
+                });
+            });
+        });
+    });
 });
