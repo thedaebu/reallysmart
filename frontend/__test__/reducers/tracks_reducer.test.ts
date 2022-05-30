73c5ff2362dde7ec70701cbb585c3818ba424dd1
thedaebu <min.kim163@gmail.com>
2022-05-30 18:57:55 -0400

    rename test data variables

--- frontend/__test__/reducers/tracks_reducer.test.ts
+++ frontend/__test__/reducers/tracks_reducer.test.ts
@@ -6,9 +6,9 @@ import tracksReducer from "../../reducers/tracks_reducer";
 import { testTrackData1, testTrackData2, testTracksData } from "../test_store_data";
 
 describe("tracks reducer", () => {
-    const track1: { [key:number]: Track } = testTrackData1;
-    const track2: { [key:number]: Track } = testTrackData2;
-    const tracks: { [key:number]: IndexTrack }= testTracksData;
+    const testTrack1: { [key:number]: Track } = testTrackData1;
+    const testTrack2: { [key:number]: Track } = testTrackData2;
+    const testTracks: { [key:number]: IndexTrack }= testTracksData;
 
     test("exports a function", () => {
         expect(typeof tracksReducer).toEqual("function");
@@ -18,51 +18,51 @@ describe("tracks reducer", () => {
             expect(tracksReducer(undefined, {})).toEqual({});
         });
         test("returns the previous state if an action is not matched", () => {
-            const state = tracksReducer(track1, { type: "NONTRACK_ACTION"});
-            expect(state).toEqual(track1);
+            const state = tracksReducer(testTrack1, { type: "NONTRACK_ACTION"});
+            expect(state).toEqual(testTrack1);
         });
         describe("RECEIVE_TRACKS action", () => {
             test("returns track index data", () => {
-                const state: { [key:number]: Track | IndexTrack } = tracksReducer(track1, { type: "RECEIVE_TRACKS", tracks });
-                expect(state).not.toEqual(track1);
-                expect(state).toEqual(tracks);
+                const state: { [key:number]: Track | IndexTrack } = tracksReducer(testTrack1, { type: "RECEIVE_TRACKS", tracks: testTracks });
+                expect(state).not.toEqual(testTrack1);
+                expect(state).toEqual(testTracks);
             });
             test("does not modify the previous state", () => {
-                const state: { [key:number]: Track | IndexTrack } = tracksReducer(track1, { type: "RECEIVE_TRACKS", tracks });
-                expect(track1).toEqual(testTrackData1);
+                const state: { [key:number]: Track | IndexTrack } = tracksReducer(testTrack1, { type: "RECEIVE_TRACKS", tracks: testTracks });
+                expect(testTrack1).toEqual(testTrackData1);
             });
         });
         describe("RECEIVE_TRACK action", () => {
             test("returns track show data", () => {
-                const state: { [key:number]: Track | IndexTrack } = tracksReducer(tracks, { type: "RECEIVE_TRACK", track: track1[1]});
-                expect(state).not.toEqual(tracks);
-                expect(state).toEqual(track1);
+                const state: { [key:number]: Track | IndexTrack } = tracksReducer(testTracks, { type: "RECEIVE_TRACK", track: testTrack1[1]});
+                expect(state).not.toEqual(testTracks);
+                expect(state).toEqual(testTrack1);
             });
             test("removes data for all other tracks except the current track", () => {
-                const state: { [key:number]: Track | IndexTrack } = tracksReducer(track1, { type: "RECEIVE_TRACK", track: track2[2]});
+                const state: { [key:number]: Track | IndexTrack } = tracksReducer(testTrack1, { type: "RECEIVE_TRACK", track: testTrack2[2]});
                 expect(state[1]).not.toBeDefined();
                 expect(state[2]).toBeDefined();
             });
             test("does not modify the previous state", () => {
-                const state: { [key:number]: Track | IndexTrack } = tracksReducer(track1, { type: "RECEIVE_TRACK", track: track2[2]});
-                expect(track1).toEqual(testTrackData1);
+                const state: { [key:number]: Track | IndexTrack } = tracksReducer(testTrack1, { type: "RECEIVE_TRACK", track: testTrack2[2]});
+                expect(testTrack1).toEqual(testTrackData1);
             });
         });
     });
-    describe("dispatches to store", () => {
+    describe("dispatch to store", () => {
         let testStore: Store;
         beforeAll(() => {
             testStore = createStore(rootReducer);
         });
-        test("contains the correct data for RECEIVE_TRACKS action", () => {
-            testStore.dispatch({ type: "RECEIVE_TRACKS", tracks} );
-            expect(testStore.getState().entities.tracks).toEqual(tracks);
+        test("contains the correct tracks data for RECEIVE_TRACKS action", () => {
+            testStore.dispatch({ type: "RECEIVE_TRACKS", tracks: testTracks });
+            expect(testStore.getState().entities.tracks).toEqual(testTracks);
         });
-        test("contains the correct data for RECEIVE_TRACK action", () => {
-            testStore.dispatch({ type: "RECEIVE_TRACK", track: track1[1]} );
-            expect(testStore.getState().entities.tracks).toEqual(track1);
-            testStore.dispatch({ type: "RECEIVE_TRACK", track: track2[2]} );
-            expect(testStore.getState().entities.tracks).toEqual(track2);
+        test("contains the correct track data for RECEIVE_TRACK action", () => {
+            testStore.dispatch({ type: "RECEIVE_TRACK", track: testTrack1[1] });
+            expect(testStore.getState().entities.tracks).toEqual(testTrack1);
+            testStore.dispatch({ type: "RECEIVE_TRACK", track: testTrack2[2] });
+            expect(testStore.getState().entities.tracks).toEqual(testTrack2);
         });
     });
 });
