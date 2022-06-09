9a92d340ed0178539c80df789f5608699f15e568
thedaebu <min.kim163@gmail.com>
2022-06-09 08:53:37 -0400

    add default value for state argument

--- frontend/reducers/index_tracks_reducer.ts
+++ frontend/reducers/index_tracks_reducer.ts
@@ -1,7 +1,7 @@
 import { RECEIVE_TRACK, RECEIVE_TRACKS } from "../actions/track_actions";
 import { Action, IndexTrack } from "../my_types";
 
-const indexTracksReducer = (state: {[key: number]: IndexTrack}, action: Action) => {
+const indexTracksReducer = (state: {[key: number]: IndexTrack} = {}, action: Action) => {
     Object.freeze(state);
 
     switch(action.type) {
