bbe8c4f8ad4a4934219891fe15e495c94bfe593c
thedaebu <min.kim163@gmail.com>
2022-06-14 15:53:14 -0400

    add track reducer

--- frontend/reducers/entities_reducer.ts
+++ frontend/reducers/entities_reducer.ts
@@ -3,7 +3,7 @@ import annotationsReducer from "./annotations_reducer";
 import commentsReducer from "./comments_reducer";
 import indexTracksReducer from "./index_tracks_reducer";
 import searchesReducer from "./searches_reducer";
-import trackReducer from "./tracks_reducer";
+import trackReducer from "./track_reducer";
 import userReducer from "./user_reducer";
 import votesReducer from "./votes_reducer";
 
