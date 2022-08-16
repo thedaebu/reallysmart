c660546adca70537f066816c3228ace6b3d2c5d7
thedaebu <min.kim163@gmail.com>
2022-08-16 17:51:42 -0400

    create useDebounce hook

--- frontend/hooks/debounce_hook.ts
+++ frontend/hooks/debounce_hook.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function useDebounce(val: string) {
     const [debounceVal, setDebounceVal] = useState<string>("");
