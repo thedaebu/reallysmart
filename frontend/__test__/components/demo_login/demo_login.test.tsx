601c8ada2aa7f5df3aef1fdfb0f363129f83e9b7
thedaebu <min.kim163@gmail.com>
2022-09-04 12:49:11 -0400

    add demo login tests

--- frontend/__test__/components/demo_login/demo_login.test.tsx
+++ frontend/__test__/components/demo_login/demo_login.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { cleanup, render, screen, within } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { cleanup, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import * as reactRedux from "react-redux";
