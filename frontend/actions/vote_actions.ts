84428052309f8eedd8b4db3cc6f9bd6ebb1a3359
thedaebu <min.kim163@gmail.com>
2022-09-25 23:17:28 -0400

    remove redux related vote data requirements

--- frontend/actions/vote_actions.ts
+++ frontend/actions/vote_actions.ts
@@ -1,33 +1,13 @@
-import { Dispatch } from "react";
-import { AnyAction } from "redux";
-import { CreatedVote, ReceivedVote, Vote } from "../my_types";
+import { CreatedVote } from "../my_types";
 import * as VoteAPIUtil from "./../util/api/vote_api_util";
 
-export const RECEIVE_VOTE = "RECEIVE_VOTE";
-export const REMOVE_VOTE = "REMOVE_VOTE";
-
-const receiveVote = ({ vote }: {vote: Vote}) => {
-    return ({
-        type: RECEIVE_VOTE,
-        vote
-    });
-};
-const removeVote = (voteId: number) => {
-    return({
-        type: REMOVE_VOTE,
-        voteId
-    });
-};
-
-export const createVote = (createdVote: CreatedVote) => (dispatch: Dispatch<AnyAction>) => {
+export const createVote = (createdVote: CreatedVote) => {
     return (
         VoteAPIUtil.createVote(createdVote)
-            .then((receivedVote: ReceivedVote) => dispatch(receiveVote(receivedVote)))
     );
 };
-export const deleteVote = (voteId: number) => (dispatch: Dispatch<AnyAction>) => {
+export const deleteVote = (voteId: number) => {
     return (
         VoteAPIUtil.deleteVote(voteId)
-            .then(() => dispatch(removeVote(voteId)))
     );
 };
