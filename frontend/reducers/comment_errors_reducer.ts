08802f05424bf02992cc6e9503149061e9ada8bb
thedaebu <min.kim163@gmail.com>
2022-10-05 21:30:34 -0400

    add clear errors case

--- frontend/reducers/comment_errors_reducer.ts
+++ frontend/reducers/comment_errors_reducer.ts
@@ -1,4 +1,4 @@
-import { RECEIVE_COMMENT_ERRORS } from "../actions/comment_actions";
+import { CLEAR_COMMENT_ERRORS, RECEIVE_COMMENT_ERRORS } from "../actions/comment_actions";
 import { Action } from "../my_types";
 
 const commentErrorsReducer = (state: Array<string> = [], action: Action) => {
@@ -7,6 +7,8 @@ const commentErrorsReducer = (state: Array<string> = [], action: Action) => {
     switch (action.type) {
         case RECEIVE_COMMENT_ERRORS:
             return action.errors;
+        case CLEAR_COMMENT_ERRORS:
+            return [];
         default:
             return state;
     }
