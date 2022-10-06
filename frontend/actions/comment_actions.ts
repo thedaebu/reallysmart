652400e5a39aca1c6c97f1b9a07fce414b5471e3
thedaebu <min.kim163@gmail.com>
2022-10-05 21:27:27 -0400

    add receiveCommentErrors and clearCommentErrors

--- frontend/actions/comment_actions.ts
+++ frontend/actions/comment_actions.ts
@@ -4,12 +4,20 @@ import { Comment, CreatedComment, ReceivedComment, UpdatedComment } from "../my_
 import * as CommentAPIUtil from "./../util/api/comment_api_util";
 
 export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
+export const RECEIVE_COMMENT_ERRORS = "RECEIVE_COMMENT_ERRORS";
+export const CLEAR_COMMENT_ERRORS = "CLEAR_COMMENT_ERRORS";
 export const REMOVE_COMMENT = "REMOVE_COMMENT";
 
 const receiveComment = ({ comment }: {comment: Comment}) => {
     return ({
         type: RECEIVE_COMMENT,
         comment
+    });
+};
+const receiveCommentErrors = (errors: Array<string>) => {
+    return ({
+        type: RECEIVE_COMMENT_ERRORS,
+        errors
     });
 };
 const removeComment = (commentId: number) => {
@@ -28,18 +36,21 @@ export const fetchComment = (commentId: number) => (dispatch: Dispatch<AnyAction
 export const createComment = (createdComment: CreatedComment) => (dispatch: Dispatch<AnyAction>) => {
     return (
         CommentAPIUtil.createComment(createdComment)
-            .then((receivedComment: ReceivedComment) => dispatch(receiveComment(receivedComment)))
+            .then((receivedComment: ReceivedComment) => dispatch(receiveComment(receivedComment)), (errors: JQuery.jqXHR) => dispatch(receiveCommentErrors(errors.responseJSON)))
     );
 };
 export const updateComment = (updatedComment: UpdatedComment) => (dispatch: Dispatch<AnyAction>) => {
     return (
         CommentAPIUtil.updateComment(updatedComment)
-            .then((receivedComment: ReceivedComment) => dispatch(receiveComment(receivedComment)))
+            .then((receivedComment: ReceivedComment) => dispatch(receiveComment(receivedComment)), (errors: JQuery.jqXHR) => dispatch(receiveCommentErrors(errors.responseJSON)))
     );
 };
 export const deleteComment = (commentId: number) => (dispatch: Dispatch<AnyAction>) => {
     return (
         CommentAPIUtil.deleteComment(commentId)
-            .then(() => dispatch(removeComment(commentId)))
+            .then(() => dispatch(removeComment(commentId)), (errors: JQuery.jqXHR) => dispatch(receiveCommentErrors(errors.responseJSON)))
     );
 };
+export const clearCommentErrors = () => ({
+    type: CLEAR_COMMENT_ERRORS
+});
