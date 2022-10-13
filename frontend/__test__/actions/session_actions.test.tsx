adb3ccfa64b96a94ef3afa80391281a4e6e06b4d
thedaebu <min.kim163@gmail.com>
2022-10-12 20:57:48 -0400

    change CLEAR_ERRORS to CLEAR_SESSION_ERRORS

--- frontend/__test__/actions/session_actions.test.tsx
+++ frontend/__test__/actions/session_actions.test.tsx
@@ -18,8 +18,8 @@ describe("session actions", () => {
         test("exports a RECEIVE_SESSION_ERRORS constant", () => {
             expect(SessionActions.RECEIVE_SESSION_ERRORS).toEqual("RECEIVE_SESSION_ERRORS");
         });
-        test("exports a CLEAR_ERRORS constant", () => {
-            expect(SessionActions.CLEAR_ERRORS).toEqual("CLEAR_ERRORS");
+        test("exports a CLEAR_SESSION_ERRORS constant", () => {
+            expect(SessionActions.CLEAR_SESSION_ERRORS).toEqual("CLEAR_SESSION_ERRORS");
         });
     });
     describe("functions", () => {
@@ -78,8 +78,8 @@ describe("session actions", () => {
             test("is exported", () => {
                 expect(typeof SessionActions.clearSessionErrors).toEqual("function");
             });
-            test("dispatches CLEAR_ERRORS when login is called", () => {
-                const actions: any = [{type: "CLEAR_ERRORS"}];
+            test("dispatches CLEAR_SESSION_ERRORS when login is called", () => {
+                const actions: any = [{type: "CLEAR_SESSION_ERRORS"}];
                 store.dispatch(SessionActions.clearSessionErrors());
                 expect(store.getActions()).toEqual(actions);
             });
