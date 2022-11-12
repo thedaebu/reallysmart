a9f18f527c9494260df0aab66ee310cffca014c6
thedaebu <min.kim163@gmail.com>
2022-11-12 14:47:30 -0500

    remove tag type because unnecessary

--- app/graphql/types/track_type.rb
+++ app/graphql/types/track_type.rb
@@ -8,16 +8,12 @@ module Types
     field :lyrics, String, null: false
     field :spotify_path, String, null: false
     field :title, String, null: false
-    field :tags, [Types::TagType], null: false
     field :total_annotations, [Types::AnnotationType], null: false
     field :total_comments, [Types::CommentType], null: false
     field :total_votes, [Types::VoteType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    def tags
-      object.tags
-    end
     def total_annotations
       object.annotations
     end
