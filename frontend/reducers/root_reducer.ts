66c33374809d66d0e3ce67963f687f260c5bddc6
thedaebu <min.kim163@gmail.com>
2022-11-19 10:42:16 -0500

    remove modal reducers because unnecessary

--- frontend/reducers/root_reducer.ts
+++ frontend/reducers/root_reducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import entitiesReducer from "./entities_reducer";
-import modalReducer from "./modal_reducer";
 import sessionReducer from "./session_reducer";
 
 const rootReducer = combineReducers({
     entities: entitiesReducer,
-    session: sessionReducer,
-    modal: modalReducer
+    session: sessionReducer
 });
 
 export default rootReducer;
