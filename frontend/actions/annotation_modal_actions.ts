45a6bf747f4fd3e57bf410fb8901278f5fdefe6b
thedaebu <min.kim163@gmail.com>
2022-11-19 10:55:21 -0500

    remove annotationModal actions because unnecessary

--- frontend/actions/annotation_modal_actions.ts
+++
@@ -1,9 +0,0 @@
-export const OPEN_ANNOTATION_MODAL: string = "OPEN_ANNOTATION_MODAL";
-export const CLOSE_ANNOTATION_MODAL: string = "CLOSE_ANNOTATION_MODAL";
-
-export const openAnnotationModal: Function = () => ({
-    type: OPEN_ANNOTATION_MODAL
-});
-export const closeAnnotationModal: Function = () => ({
-    type: CLOSE_ANNOTATION_MODAL
-});
