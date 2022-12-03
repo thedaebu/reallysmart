a1ae49fa9bb54d65ba617e57f0096cee824d2655
thedaebu <min.kim163@gmail.com>
2022-12-03 11:30:47 -0500

    add annotation notification migration

---
+++ db/migrate/20221203160949_create_annotation_notifications.rb
@@ -0,0 +1,11 @@
+class CreateAnnotationNotifications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :annotation_notifications do |t|
+      t.bigint :commenter_id, null: false
+      t.timestamps
+    end
+    add_index :annotation_notifications, :commenter_id
+    add_reference :annotation_notifications, :annotation, index: true
+    add_reference :annotation_notifications, :comment, index: true
+  end
+end
