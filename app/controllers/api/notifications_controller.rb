ee23e3174b41f2cca72f8454a7306a8fdfb28468
thedaebu <min.kim163@gmail.com>
2022-12-20 20:08:02 -0500

    add update logic for read column for annotation notifications

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,2 +1,9 @@
 class Api::NotificationsController < ApplicationController
+    def update
+        if params[:notification][:type] === "Annotation"
+            annotation_notification = AnnotationNotification.find(params[:id])
+            annotation_notification.read = true
+            annotation_notification.save
+        end
+    end
 end
