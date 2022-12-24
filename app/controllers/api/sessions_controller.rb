76b5f68d27dd2172c8d78fa4d7648902686fa8c1
thedaebu <min.kim163@gmail.com>
2022-12-24 02:22:40 -0500

    rename variables to signify annotation alert and mention

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,22 +4,22 @@ class Api::SessionsController < ApplicationController
         if user
             login!(user)
             @user = user.slice(:id, :username)
-            annotation_alerts = user.annotation_alerts.map do |notification|
-                temp = notification.slice(:created_at, :id, :read)
-                temp[:body] = notification.annotation.body
-                temp[:commenter] = notification.commenter.username
-                temp[:track] = notification.annotation.track.slice(:artist, :title)
-                temp[:type] = "AnnotationAlert"
-                temp
+            annotation_alerts = user.annotation_alerts.map do |annotation_alert|
+                temp_annotation_alert = annotation_alert.slice(:created_at, :id, :read)
+                temp_annotation_alert[:body] = annotation_alert.annotation.body
+                temp_annotation_alert[:commenter] = annotation_alert.commenter.username
+                temp_annotation_alert[:track] = annotation_alert.annotation.track.slice(:artist, :title)
+                temp_annotation_alert[:type] = "AnnotationAlert"
+                temp_annotation_alert
             end
-            mentions = user.mentions.map do |notification|
-                temp = notification.slice(:created_at, :id, :read)
-                temp[:body] = notification.mentionable_type == "Annotation" ? notification.mentionable.body : ""
-                temp[:mentionable_type] = notification.mentionable_type
-                temp[:mentioner] = notification.mentioner.username
-                temp[:track] = notification.mentionable_type == "Track" ? notification.mentionable.slice(:artist, :title) : notification.mentionable.track.slice(:artist, :title)
-                temp[:type] = "Mention"
-                temp
+            mentions = user.mentions.map do |mention|
+                temp_mention = mention.slice(:created_at, :id, :read)
+                temp_mention[:body] = mention.mentionable_type == "Annotation" ? mention.mentionable.body : ""
+                temp_mention[:mentionable_type] = mention.mentionable_type
+                temp_mention[:mentioner] = mention.mentioner.username
+                temp_mention[:track] = mention.mentionable_type == "Track" ? mention.mentionable.slice(:artist, :title) : mention.mentionable.track.slice(:artist, :title)
+                temp_mention[:type] = "Mention"
+                temp_mention
             end
             @user[:notifications] = annotation_alerts.concat(mentions)
             # avatar_url = url_for(user.avatar)
