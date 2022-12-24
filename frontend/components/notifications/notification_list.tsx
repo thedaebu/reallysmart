6eac39bc11c634b145e8a3f4ce709274d899e1f3
thedaebu <min.kim163@gmail.com>
2022-12-24 18:55:03 -0500

    finish display for notification list when opened

--- frontend/components/notifications/notification_list.tsx
+++ frontend/components/notifications/notification_list.tsx
@@ -2,17 +2,17 @@ import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { RiCheckboxIndeterminateFill } from "react-icons/ri"
 import * as NotificationActions from "./../../actions/notification_actions";
-import { AnnotationNotification } from '../../my_types';
+import { AnnotationAlert, Mention } from '../../my_types';
 
 type Props = {
     changeNotificationOpenStatus: Function,
     makeReadStatusTrue: Function,
-    notifications: Array<AnnotationNotification>
+    notifications: Array<AnnotationAlert | Mention>
 }
 
 function NotificationList(props: Props) {
     const { changeNotificationOpenStatus, makeReadStatusTrue, notifications } = props;
-
+    console.log(notifications)
     useEffect(() => {
         makeReadStatusTrue();
         updateNotifications();
@@ -22,13 +22,12 @@ function NotificationList(props: Props) {
 
     function updateNotifications() {
         for (let i = 0; i < notifications.length; i++) {
-            let notification: any = notifications[i];
+            let notification: AnnotationAlert | Mention = notifications[i];
             if (notification.read === true) break;
             const updatedNotification = {
                 id: notification.id,
-                type: "Annotation"
+                type: notification.type === "AnnotationAlert" ? "AnnotationAlert" : "Mention"
             }
-            notification["type"] = "Annotation";
             updateNotification(updatedNotification);
         }
     }
@@ -37,21 +36,40 @@ function NotificationList(props: Props) {
         changeNotificationOpenStatus();
     }
 
-    function notificationItem(notification: AnnotationNotification) {
-        const { body, commenter, track } = notification;
-        const { artist, title } = track;
+    function notificationItem(notification: AnnotationAlert | Mention) {
+        if (notification.type === "AnnotationAlert"){
+            const { body, commenter, track } = notification;
+            const { artist, title } = track;
 
-        return (
-            <Link to={`/tracks/${urlify(artist)}__${urlify(title)}`}>
-                <span className="notification-list__item-highlighted">{`${commenter} `}</span>
-                commented on your annotation '
-                <span className="notification-list__item-highlighted">{`${(notificationify(body))}`}</span>
-                ' for
-                <span className="notification-list__item-highlighted">{` ${artist} - ${title}`}</span>
-            </Link>
-        );
+            return (
+                <Link to={`/tracks/${urlify(artist)}__${urlify(title)}`}>
+                    <span className="notification-list__item-highlighted">{`${commenter} `}</span>
+                    commented on your annotation
+                    <span className="notification-list__item-highlighted">{` '${(notificationify(body))}' `}</span>
+                    for
+                    <span className="notification-list__item-highlighted">{` ${artist} - ${title}`}</span>
+                </Link>
+            );
+        } else {
+            const { body, mentioner, track } = notification;
+            const { artist, title } = track;
+
+            return (
+                <Link to={`/tracks/${urlify(artist)}__${urlify(title)}`}>
+                    <span className="notification-list__item-highlighted">{`${mentioner} `}</span>
+                    mentioned you in a comment
+                    {body.length > 0 &&
+                        <span>
+                            {' for the annotation '}<span className="notification-list__item-highlighted">{`'${(notificationify(body))}' `}</span>
+                        </span>
+                    }
+                    {' for'}
+                    <span className="notification-list__item-highlighted">{` ${artist} - ${title}`}</span>
+                </Link>
+            );
+        }
     }
-    
+
     function urlify(string: string) {
         const words: Array<string> = string.split(" ");
         return words.join("_").toLowerCase();
@@ -74,7 +92,7 @@ function NotificationList(props: Props) {
                 />
             </div>
             <ul>
-                {notifications.map((notification: AnnotationNotification, idx: number) => {
+                {notifications.map((notification: AnnotationAlert | Mention, idx: number) => {
                     return (
                         <li className="notification-list__item" key={idx}>
                             {notificationItem(notification)}
