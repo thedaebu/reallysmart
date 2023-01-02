6a64b47fadc8fa9fbb0f4c7abf8a60d6ae6a428a
thedaebu <min.kim163@gmail.com>
2023-01-02 15:32:55 -0500

    remove annotator_name column

--- db/migrate/20210320201935_create_annotations.rb
+++ db/migrate/20210320201935_create_annotations.rb
@@ -3,7 +3,6 @@ class CreateAnnotations < ActiveRecord::Migration[5.2]
     create_table :annotations do |t|
       t.text :body, null: false
       t.bigint :annotator_id, null: false
-      t.string :annotator_name, null: false
       t.integer :end_index, null: false
       t.integer :start_index, null: false
       t.timestamps
