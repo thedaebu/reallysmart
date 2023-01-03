5afbd7cdd19a5b40be2c9890940b065743722818
thedaebu <min.kim163@gmail.com>
2023-01-03 17:49:49 -0500

    rename mentioner to mentioner_name

--- app/graphql/types/mention_type.rb
+++ app/graphql/types/mention_type.rb
@@ -5,7 +5,7 @@ module Types
     field :body, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :id, ID, null: false
-    field :mentioner, String, null: false
+    field :mentioner_name, String, null: false
     field :read, Boolean, null: false
     field :track, Types::TrackInfoType, null: false
     field :type, String, null: false
@@ -13,7 +13,7 @@ module Types
     def body
       object.commentable_type == "Track" ? "" : object.commentable.body
     end
-    def mentioner
+    def mentioner_name
       object.mentioner.username
     end
     def track
