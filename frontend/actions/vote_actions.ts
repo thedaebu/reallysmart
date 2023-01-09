aa876653e672d6f88116cfab5e8448c06114ac90
thedaebu <min.kim163@gmail.com>
2023-01-09 18:55:44 -0500

    change createVote and deleteVote to be from vote api util because no need to be from action

--- frontend/actions/vote_actions.ts
+++
@@ -1,9 +0,0 @@
-import { CreatedVote } from "../my_types";
-import * as VoteAPIUtil from "./../util/api/vote_api_util";
-
-export const createVote: Function = (createdVote: CreatedVote) => (
-    VoteAPIUtil.createVote(createdVote)
-);
-export const deleteVote: Function = (voteId: number) => (
-    VoteAPIUtil.deleteVote(voteId)
-);
