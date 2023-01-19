4bcbbb10a3ed7cb9151dbd95404642fa1ce9084a
thedaebu <min.kim163@gmail.com>
2023-01-19 13:00:08 -0500

    add method specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,50 @@ RSpec.describe User, type: :model do
         it { should have_many(:votes) }
     end
     describe "methods" do
-        
+        describe "password encryption" do
+            it "does not save passwords to the database" do
+                User.create!(username: "sunnyd", password: "password")
+                sunny = User.find_by(username: "sunnyd")
+                expect(sunny.password).to be(nil)
+            end
+            describe "it saves passwords properly" do 
+                it "encrypts the password using BCrypt" do
+                    expect(BCrypt::Password).to receive(:create)
+                    User.new(username: "jackbruce", password: "abcdef")
+                end
+                it "properly sets the password reader" do
+                    user = User.new(username: "jackbruce", password: "abcdef")
+                    expect(user.password).to eq("abcdef")
+                end
+            end
+        end
+        describe "session token" do
+            subject { User.create!(username: "lillyllama", password: "password") }
+            it "assigns a session_token if one is not given" do
+                expect(subject.session_token).not_to be_nil
+            end
+            it "resets a session token on a user" do
+                old_session_token = subject.session_token
+                new_session_token = subject.reset_session_token!
+                expect(old_session_token).not_to eq(new_session_token)
+            end
+        end
+        describe "finds users by credentials" do
+            describe "with a valid username and password" do
+                it "should return the proper user" do
+                    sally = User.create(username: "sallylee", password: "password")
+                    user = User.find_by_credentials("sallylee","password")
+                    expect(sally.username).to eq(user.username)
+                    expect(sally.password_digest).to eq(user.password_digest)
+                end
+            end
+            describe "with an invalid username and password" do
+                it "should return nil" do
+                    jack = User.create(username: "jackbruce", password: "abcdef")
+                    user = User.find_by_credentials("jackbruce","notthepassword")
+                    expect(user).to be_nil
+                end
+            end
+        end
     end
 end
