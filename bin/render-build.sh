3c272e34a5f2f3c3dc58ab0fc0c7a5ddd6a57b5f
thedaebu <min.kim163@gmail.com>
2023-01-23 17:05:07 -0500

    update commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,9 @@
 set -o errexit
 
 bundle install
+npm install
+npm run build
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rails db:seed
+rails db:seed
