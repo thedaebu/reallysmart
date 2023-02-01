2489595006f6be112e911acc1683bbaa693d736e
thedaebu <min.kim163@gmail.com>
2023-02-01 16:30:56 -0500

    remove session reducer tests

--- frontend/__test__/reducers/root_reducer.test.ts
+++ frontend/__test__/reducers/root_reducer.test.ts
@@ -2,7 +2,6 @@ import { createStore } from "redux";
 import { Store } from "../../store/store";
 import entitiesReducer from "../../reducers/entities_reducer";
 import rootReducer from "../../reducers/root_reducer";
-import sessionReducer from "../../reducers/session_reducer";
 
 describe("root reducer", () => {
     let testStore: Store;
@@ -18,14 +17,6 @@ describe("root reducer", () => {
         });
         test("is included in the entities key", () => {
             expect(testStore.getState().entities).toBeDefined();
-        });
-    });
-    describe("session reducer", () => {
-        test("exports a function", () => {
-            expect(typeof sessionReducer).toEqual("function");
-        });
-        test("is included in the errors key", () => {
-            expect(testStore.getState().session).toBeDefined();
         });
     });
 });
