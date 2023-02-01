5e0d829dd39ca6b9e8ba34b510208407f7b2b0db
thedaebu <min.kim163@gmail.com>
2023-02-01 16:00:28 -0500

    change RECEIVE_CURRENT_USER to return data without number key

--- frontend/reducers/user_reducer.ts
+++ frontend/reducers/user_reducer.ts
@@ -1,14 +1,14 @@
 import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
 import { SessionAction, User } from "../my_types";
 
-const userReducer = (state: {[key: number]: User} = {}, action: SessionAction) => {
+const userReducer = (state: User | null = null, action: SessionAction) => {
     Object.freeze(state);
 
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return {[action.user.id]: action.user};
+            return action.user;
         case LOGOUT_CURRENT_USER:
-            return {};
+            return null;
         default:
             return state;
     }
