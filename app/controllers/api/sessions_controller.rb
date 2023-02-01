ed48b6e755aeea1ff26aa31bd402778468936506
thedaebu <min.kim163@gmail.com>
2023-02-01 16:03:46 -0500

    change indentations

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,47 +1,49 @@
 class Api::SessionsController < ApplicationController
-    def create
-        user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-        if user
-            login!(user)
-            @user = user.slice(:id, :username)
-            annotation_alerts = user.annotation_alerts.map do |annotation_alert|
-                annotation = annotation_alert.annotation
+  def create
+    user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if user
+      login!(user)
+      @user = user.slice(:id, :username)
+      annotation_alerts = user.annotation_alerts.map do |annotation_alert|
+        annotation = annotation_alert.annotation
+        temp_annotation_alert = annotation_alert.slice(:created_at, :id, :read)
 
-                temp_annotation_alert = annotation_alert.slice(:created_at, :id, :read)
-                temp_annotation_alert[:body] = annotation.body
-                temp_annotation_alert[:commenter_name] = annotation_alert.commenter.username
-                temp_annotation_alert[:track] = annotation.track.slice(:artist, :title)
-                temp_annotation_alert[:type] = "AnnotationAlert"
-                temp_annotation_alert
-            end
-            mentions = user.mentions.map do |mention|
-                comment = mention.comment
-                commentable_type = comment.commentable_type
+        temp_annotation_alert[:body] = annotation.body
+        temp_annotation_alert[:commenter_name] = annotation_alert.commenter.username
+        temp_annotation_alert[:track] = annotation.track.slice(:artist, :title)
+        temp_annotation_alert[:type] = "AnnotationAlert"
 
-                temp_mention = mention.slice(:created_at, :id, :read)
-                temp_mention[:body] = commentable_type == "Track" ? "" : comment.commentable.body
-                temp_mention[:mentioner_name] = mention.mentioner.username
-                temp_mention[:track] = commentable_type == "Track" ? comment.commentable.slice(:artist, :title) : comment.commentable.track.slice(:artist, :title)
-                temp_mention[:type] = "Mention"
-                temp_mention
-            end
-            @user[:annotation_alerts] = annotation_alerts
-            @user[:mentions] = mentions
-            # avatar_url = url_for(user.avatar)
+        temp_annotation_alert
+      end
+      mentions = user.mentions.map do |mention|
+        comment = mention.comment
+        commentable_type = comment.commentable_type
+        temp_mention = mention.slice(:created_at, :id, :read)
 
-            result = {:user => @user}
-            render json: result
-        else
-            render json: ["Invalid credentials"], status: 401
-        end
+        temp_mention[:body] = commentable_type == "Track" ? "" : comment.commentable.body
+        temp_mention[:mentioner_name] = mention.mentioner.username
+        temp_mention[:track] = commentable_type == "Track" ? comment.commentable.slice(:artist, :title) : comment.commentable.track.slice(:artist, :title)
+        temp_mention[:type] = "Mention"
+
+        temp_mention
+      end
+      @user[:annotation_alerts] = annotation_alerts
+      @user[:mentions] = mentions
+      # avatar_url = url_for(user.avatar)
+
+      result = {:user => @user}
+      render json: result
+    else
+      render json: ["Invalid credentials"], status: 401
     end
+  end
 
-    def destroy
-        if !!current_user
-            logout!
-            render json: {}
-        else
-            render json: ['You are not logged in.'], status: 404
-        end
+  def destroy
+    if !!current_user
+      logout!
+      render json: {}
+    else
+      render json: ['You are not logged in.'], status: 404
     end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,44 +1,44 @@
 class User < ApplicationRecord
-    validates_presence_of :password_digest, :session_token, :username
-    validates :password, allow_nil: true, length: { in: 6..20 }, format: { with: /\A[a-z0-9A-Z ]+\z/ }
-    validates :username, format: { with: /\A[a-z0-9A-Z ]+\z/ }, length: { in: 6..20 }, uniqueness: true
+  validates_presence_of :password_digest, :session_token, :username
+  validates :password, allow_nil: true, length: { in: 6..20 }, format: { with: /\A[a-z0-9A-Z ]+\z/ }
+  validates :username, format: { with: /\A[a-z0-9A-Z ]+\z/ }, length: { in: 6..20 }, uniqueness: true
 
-    attr_reader :password
+  attr_reader :password
 
-    after_initialize :ensure_session_token
+  after_initialize :ensure_session_token
 
-    has_many :annotations, foreign_key: :annotator_id, class_name: "Annotation"
-    has_many :annotation_alerts, through: :annotations, source: :alerts
-    has_many :comments, foreign_key: :commenter_id, class_name: "Comment"
-    has_many :mentions, foreign_key: :mentionee_id, class_name: "Mention"
-    has_many :mentioneds, foreign_key: :mentioner_id, class_name: "Mention"
-    has_many :votes, foreign_key: :voter_id, class_name: "Vote"
+  has_many :annotations, foreign_key: :annotator_id, class_name: "Annotation"
+  has_many :annotation_alerts, through: :annotations, source: :alerts
+  has_many :comments, foreign_key: :commenter_id, class_name: "Comment"
+  has_many :mentions, foreign_key: :mentionee_id, class_name: "Mention"
+  has_many :mentioneds, foreign_key: :mentioner_id, class_name: "Mention"
+  has_many :votes, foreign_key: :voter_id, class_name: "Vote"
 
-    # has_one_attached :avatar
+  # has_one_attached :avatar
 
-    def self.find_by_credentials(username, password)
-        user = User.find_by(username: username)
-        return nil unless user
-        user.is_password?(password) ? user : nil
-    end
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
 
-    def password=(password)
-        @password = password
-        self.password_digest = BCrypt::Password.create(password)
-    end
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
-    def is_password?(password)
-        BCrypt::Password.new(self.password_digest).is_password?(password)
-    end
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
-    def reset_session_token!
-        self.session_token = SecureRandom.urlsafe_base64(16)
-        self.save!
-        self.session_token
-    end
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
 
-    private
-    def ensure_session_token
-        self.session_token ||= SecureRandom.urlsafe_base64(16)
-    end
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
 end
