6c52b551feadf6860459c88e349855adf13e8682
thedaebu <min.kim163@gmail.com>
2023-02-01 16:02:14 -0500

    add functionality to fetch current user according to session token

--- frontend/components/app.tsx
+++ frontend/components/app.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useContext } from "react";
+import React, { Dispatch, lazy, Suspense, useContext, useEffect } from "react";
 import {
     Route,
     Switch,
@@ -6,8 +6,8 @@ import {
 } from "react-router-dom";
 import { AuthRoute } from "../util/route_util";
 import { ThemeContext } from "../contexts/theme_context";
-import { useSelector } from "react-redux";
-import { State, User } from "../my_types";
+import { useDispatch, useSelector } from "react-redux";
+import { State, User, Window } from "../my_types";
 import DemoLogin from "./demo_login/demo_login";
 import LoginForm from "./session_form/login_form";
 import SessionMenu from "./session_menu/session_menu";
@@ -15,13 +15,27 @@ import SignupForm from "./session_form/signup_form";
 import Searchbar from "./searchbar/searchbar";
 import NotificationShow from "./notifications/notification_show";
 import ThemeToggle from "./theme_toggle/theme_toggle";
+import * as SessionActions from "./../actions/session_actions";
+import { AnyAction } from "redux";
 const TrackIndex = lazy(() => import("./tracks/track_index"));
 const TrackShow = lazy(() => import("./tracks/track_show"));
 
+declare const window: Window;
+
 function App({ cableApp }: { cableApp: any}) {
-    const currentUser: User = useSelector((state: State) => state.entities.user[state.session.id]);
+    const currentUser: User = useSelector((state: State) => state.entities.user);
+
+    const dispatch: Dispatch<AnyAction> = useDispatch();
+    const fetchUser: Function = (sessionToken: string) => dispatch(SessionActions.fetchUser(sessionToken));
 
     const { theme } = useContext(ThemeContext);
+
+    useEffect(() => {
+        if (window.currentUser) {
+            fetchUser(window.currentUser.session_token);
+            delete window.currentUser
+        }
+    }, [window.currentUser]);
 
     return (
         <div className={theme === "light" ? "app" : "app--dark"}>
