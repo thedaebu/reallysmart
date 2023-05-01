e14d054056ab48e0ad206a4f142ca372f75cd812
thedaebu <min.kim163@gmail.com>
2023-05-01 12:08:41 -0400

    add tracks controller specs

--- spec/requests/tracks_spec.rb
+++ spec/requests/tracks_spec.rb
@@ -1,7 +1,38 @@
-RSpec.describe "Tracks", type: :request do
-    describe "Get /index" do
-        
-        get api_track_path
-        # expect(response).to have_http_status(:ok)
+require 'rails_helper'
+
+RSpec.describe 'Api::TracksController', type: :request do
+    before(:all) do 
+        @track = Track.create!(artist: 'testartist', title: 'testtitle', lyrics: 'testlyrics', artwork_path: 'testartwork_path', spotify_path: 'testspotify_path')
     end
+    after(:all) do
+        @track.destroy
+    end
+    describe 'Get /api/tracks' do
+        before(:all) do
+            get '/api/tracks'
+            @response = response
+            @json = JSON.parse(response.body)['tracks']["#{@track.id}"]
+        end
+        it 'can reach the url' do
+            expect(@response.status).to eq(200)
+        end
+        it 'returns something' do
+            expect(@json['title']).to eq(@track[:title])
+        end
+    end
+    # describe 'Get /api/tracks/:id' do
+        # before(:all) do
+        #     print("/api/tracks/#{@track.id.to_s}")
+        #     get "/api/tracks/#{@track.id.to_s}"
+        #     @response = response
+            # @json = JSON.parse(response.body)
+        # end
+        # it 'can reach the url' do
+        #     expect(@response.status).to eq(200)
+            # print("/api/tracks/#{@track.id}")
+        # end
+    #     if 'returns somethingg' do
+    #         expect(@json['title']).to eq('something')
+    #     end
+    # end
 end
