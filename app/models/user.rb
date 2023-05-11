2a2cc581fdd9d3c6258fbebe03891183ae904b37
thedaebu <min.kim163@gmail.com>
2023-05-11 13:20:33 -0400

    add add_notifications method

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,36 @@ class User < ApplicationRecord
   has_many :mentioneds, foreign_key: :mentioner_id, class_name: "Mention"
   has_many :votes, foreign_key: :voter_id, class_name: "Vote"
 
-  # has_one_attached :avatar
+  def self.add_notifications(user)
+    user_with_notifications = user.slice(:id, :username)
+    annotation_alerts = user.annotation_alerts.map do |annotation_alert|
+      annotation = annotation_alert.annotation
+      temp_annotation_alert = annotation_alert.slice(:created_at, :id, :read)
+
+      temp_annotation_alert[:body] = annotation.body
+      temp_annotation_alert[:commenter_name] = annotation_alert.commenter.username
+      temp_annotation_alert[:track] = annotation.track.slice(:artist, :title)
+      temp_annotation_alert[:type] = 'AnnotationAlert'
+
+      temp_annotation_alert
+    end
+    mentions = user.mentions.map do |mention|
+      comment = mention.comment
+      commentable_type = comment.commentable_type
+      temp_mention = mention.slice(:created_at, :id, :read)
+
+      temp_mention[:body] = commentable_type == 'Track' ? '' : comment.commentable.body
+      temp_mention[:mentioner_name] = mention.mentioner.username
+      temp_mention[:track] = commentable_type == 'Track' ? comment.commentable.slice(:artist, :title) : comment.commentable.track.slice(:artist, :title)
+      temp_mention[:type] = 'Mention'
+
+      temp_mention
+    end
+    user_with_notifications[:annotation_alerts] = annotation_alerts
+    user_with_notifications[:mentions] = mentions
+
+    user_with_notifications
+  end
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
