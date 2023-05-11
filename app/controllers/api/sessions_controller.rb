1862e440a708bd47581435431fe0bd2729bb03ed
thedaebu <min.kim163@gmail.com>
2023-05-11 13:21:57 -0400

    use add_notifications method in create method

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,32 +4,7 @@ class Api::SessionsController < ApplicationController
     user = User.find_by_credentials(user_info[:username], user_info[:password])
     if user
       login!(user)
-      @user = user.slice(:id, :username)
-      annotation_alerts = user.annotation_alerts.map do |annotation_alert|
-        annotation = annotation_alert.annotation
-        temp_annotation_alert = annotation_alert.slice(:created_at, :id, :read)
-
-        temp_annotation_alert[:body] = annotation.body
-        temp_annotation_alert[:commenter_name] = annotation_alert.commenter.username
-        temp_annotation_alert[:track] = annotation.track.slice(:artist, :title)
-        temp_annotation_alert[:type] = 'AnnotationAlert'
-
-        temp_annotation_alert
-      end
-      mentions = user.mentions.map do |mention|
-        comment = mention.comment
-        commentable_type = comment.commentable_type
-        temp_mention = mention.slice(:created_at, :id, :read)
-
-        temp_mention[:body] = commentable_type == 'Track' ? '' : comment.commentable.body
-        temp_mention[:mentioner_name] = mention.mentioner.username
-        temp_mention[:track] = commentable_type == 'Track' ? comment.commentable.slice(:artist, :title) : comment.commentable.track.slice(:artist, :title)
-        temp_mention[:type] = 'Mention'
-
-        temp_mention
-      end
-      @user[:annotation_alerts] = annotation_alerts
-      @user[:mentions] = mentions
+      @user = User.add_notifications(user)
 
       result = {:user => @user}
       render json: result
