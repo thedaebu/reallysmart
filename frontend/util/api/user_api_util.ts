a6c3abf988f92924a2d9ab844293b58c060b5fe2
thedaebu <min.kim163@gmail.com>
2023-05-19 13:31:02 -0400

    add udpateUser API function

--- frontend/util/api/user_api_util.ts
+++ frontend/util/api/user_api_util.ts
@@ -1,9 +1,20 @@
 import $ from "jquery";
+import { UpdatedUser } from "../../my_types";
 
 export const fetchUser: Function = (sessionToken: string) => (
     $.ajax({
         data: { sessionToken },
         method: "GET",
+        url: `api/users`
+    })
+);
+export const updateUser: Function = (updatedUser: UpdatedUser) => (
+    $.ajax({
+        data: {
+            updatedUser,
+            authenticity_token: $('[name="csrf-token"]').attr("content")
+        },
+        method: "PUT",
         url: `api/users`
     })
 );
