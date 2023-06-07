c0959872ebde85a40776c58d2536bd0f32169510
thedaebu <min.kim163@gmail.com>
2023-06-07 10:44:49 -0400

    remove fetchUser call

--- frontend/components/app.tsx
+++ frontend/components/app.tsx
@@ -1,9 +1,9 @@
-import React, { Dispatch, lazy, Suspense, useContext, useEffect } from "react";
+import React, { lazy, Suspense, useContext } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AccountRoute, SessionRoute } from "../util/route_util";
 import { ThemeContext } from "../contexts/theme_context";
-import { useDispatch, useSelector } from "react-redux";
-import { State, User, Window } from "../my_types";
+import { useSelector } from "react-redux";
+import { State, User } from "../my_types";
 import DemoLogin from "./demo_login/demo_login";
 import LoginForm from "./session_form/login_form";
 import SessionMenu from "./session_menu/session_menu";
@@ -12,27 +12,13 @@ import Searchbar from "./searchbar/searchbar";
 import NotificationShow from "./notifications/notification_show";
 import ThemeToggle from "./theme_toggle/theme_toggle";
 import AccountShow from "./account/account_show";
-import * as SessionActions from "./../actions/session_actions";
-import { AnyAction } from "redux";
 const TrackIndex = lazy(() => import("./tracks/track_index"));
 const TrackShow = lazy(() => import("./tracks/track_show"));
-
-declare const window: Window;
 
 function App({ cableApp }: { cableApp: any}) {
     const currentUser: User = useSelector((state: State) => state.entities.user);
 
-    const dispatch: Dispatch<AnyAction> = useDispatch();
-    const fetchUser: Function = (sessionToken: string) => dispatch(SessionActions.fetchUser(sessionToken));
-
     const { theme } = useContext(ThemeContext);
-
-    useEffect(() => {
-        if (window.currentUser) {
-            fetchUser(window.currentUser.session_token);
-            delete window.currentUser
-        }
-    }, [window.currentUser]);
 
     return (
         <div className={theme === "light" ? "app" : "app--dark"}>
