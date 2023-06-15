28521dbaa16556a06910298ab78a48376f096632
thedaebu <min.kim163@gmail.com>
2023-06-15 12:57:50 -0400

    add flash message component

--- frontend/components/app.tsx
+++ frontend/components/app.tsx
@@ -4,19 +4,21 @@ import { AccountRoute, SessionRoute } from "../util/route_util";
 import { ThemeContext } from "../contexts/theme_context";
 import { useSelector } from "react-redux";
 import { State, User } from "../my_types";
+import AccountShow from "./account/account_show";
 import DemoLogin from "./demo_login/demo_login";
+import FlashMessage from "./flash_message/flash_message";
 import LoginForm from "./session_form/login_form";
+import NotificationShow from "./notifications/notification_show";
+import Searchbar from "./searchbar/searchbar";
 import SessionMenu from "./session_menu/session_menu";
 import SignupForm from "./session_form/signup_form";
-import Searchbar from "./searchbar/searchbar";
-import NotificationShow from "./notifications/notification_show";
 import ThemeToggle from "./theme_toggle/theme_toggle";
-import AccountShow from "./account/account_show";
 const TrackIndex = lazy(() => import("./tracks/track_index"));
 const TrackShow = lazy(() => import("./tracks/track_show"));
 
 function App({ cableApp }: { cableApp: any}) {
     const currentUser: User = useSelector((state: State) => state.entities.user);
+    const flashMessage: string = useSelector((state: State) => state.entities.flashMessage);
 
     const { theme } = useContext(ThemeContext);
 
@@ -25,6 +27,7 @@ function App({ cableApp }: { cableApp: any}) {
             ? "app"
             : "app--dark"}
         >
+            {flashMessage && <FlashMessage flashMessage={flashMessage} />}
             <header className="header">
                 <Searchbar theme={theme} />
                 <Link to="/" className="header__logo">REALLYSMART</Link>
