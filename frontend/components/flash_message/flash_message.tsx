d9e648a266a6b62a0e75c12b0d5a53f16c04069d
thedaebu <min.kim163@gmail.com>
2023-06-15 13:56:01 -0400

    create flash message component

---
+++ frontend/components/flash_message/flash_message.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { RiCheckboxIndeterminateFill } from "react-icons/ri";
+import * as FlashMessageActions from "../../actions/flash_message_actions";
+import { AnyAction, Dispatch } from "@reduxjs/toolkit";
+
+function FlashMessage({ flashMessage }: { flashMessage: string; }) {
+    const dispatch: Dispatch<AnyAction> = useDispatch();
+    const deleteFlashMessage: Function = () => dispatch(FlashMessageActions.deleteFlashMessage());
+
+    useEffect(() => {
+        setTimeout(deleteFlashMessage, 5000);
+    }, [flashMessage]);
+
+    return (
+        <div className="flash-message">
+            <section className="flash-message__container">
+                <span className="flash-message__message">{flashMessage}</span>
+                <RiCheckboxIndeterminateFill 
+                    className="flash-message__exit"
+                    size={16}
+                    onClick={() => deleteFlashMessage()}
+                />
+                <div className="flash-message__timer"></div>
+            </section>
+        </div>
+    );
+}
+
+export default FlashMessage;
