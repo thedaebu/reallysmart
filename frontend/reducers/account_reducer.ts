707ac0acf68b73ae79d664040f9456e7ffa94418
thedaebu <min.kim163@gmail.com>
2023-06-15 13:20:38 -0400

    add flashMessage info

--- frontend/reducers/account_reducer.ts
+++ frontend/reducers/account_reducer.ts
@@ -1,8 +1,9 @@
 import { RECEIVE_ACCOUNT } from "../actions/account_actions";
 import { LOGOUT_CURRENT_USER } from "../actions/session_actions";
-import { Account, AccountAction, SessionAction } from "../my_types";
+import { AnyAction } from "@reduxjs/toolkit";
+import { Account } from "../my_types";
 
-const accountsReducer = (state: Account | {} = {}, action: AccountAction | SessionAction) => {
+const accountsReducer = (state: Account | {} = {}, action: AnyAction) => {
     Object.freeze(state);
 
     switch (action.type) {
--- frontend/reducers/flash_message_reducer.ts
+++ frontend/reducers/flash_message_reducer.ts
@@ -1,11 +1,23 @@
-import { SessionAction } from "../my_types";
+import { RECEIVE_ANNOTATION, REMOVE_ANNOTATION } from "../actions/annotation_actions";
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from "../actions/comment_actions";
 import { CLEAR_FLASH_MESSAGE } from "../actions/flash_message_actions";
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { AnyAction } from "@reduxjs/toolkit";
 
-const flashMessageReducer = (state: string = "", action: SessionAction) => {
+const flashMessageReducer = (state: string = "", action: AnyAction) => {
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return "Log In Successful.";
+            return action.flashMessage;
+        case LOGOUT_CURRENT_USER:
+            return action.flashMessage;
+        case RECEIVE_ANNOTATION:
+            return action.flashMessage;
+        case REMOVE_ANNOTATION:
+            return action.flashMessage;
+        case RECEIVE_COMMENT:
+            return action.flashMessage;
+        case REMOVE_COMMENT:
+            return action.flashMessage;
         case CLEAR_FLASH_MESSAGE:
             return "";
         default:
