bb15a52c19b4325c848b15a4c0307b4ff43f356e
thedaebu <min.kim163@gmail.com>
2023-08-12 13:56:48 -0400

    remove temporarily current_user condition

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,9 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    # identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      # self.current_user = find_verified_user
     end
 
     private
