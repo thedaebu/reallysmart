54b30d49004cd672696a61ae528cc69a76a86eef
thedaebu <min.kim163@gmail.com>
2023-08-14 11:17:17 -0400

    add broadcast_annotation calls for action methods

--- app/controllers/api/annotations_controller.rb
+++ app/controllers/api/annotations_controller.rb
@@ -1,59 +1,62 @@
 class Api::AnnotationsController < ApplicationController
-    def show
-        annotation = Annotation.find(params[:id])
-        @annotation = annotation.as_json
-        @annotation[:annotator_name] = annotation.annotator.username
-        @annotation[:votes] = {}
-        annotation.votes.each do |vote|
-            @annotation[:votes][vote.id] = vote.slice(:id, :voteable_id, :voteable_type, :voter_id)
-        end
-
-        result = {:annotation => @annotation}
-        render json: result
+  def show
+    annotation = Annotation.find(params[:id])
+    @annotation = annotation.as_json
+    @annotation[:annotator_name] = annotation.annotator.username
+    @annotation[:votes] = {}
+    annotation.votes.each do |vote|
+      @annotation[:votes][vote.id] = vote.slice(:id, :voteable_id, :voteable_type, :voter_id)
     end
 
-    def create
-        created_annotation = Annotation.new(annotation_params)
-        if created_annotation.save
-            @annotation = created_annotation.as_json
-            @annotation[:annotator_name] = created_annotation.annotator.username
-            @annotation[:votes] = {}
+    result = {:annotation => @annotation}
+    render json: result
+  end
 
-            result = {:annotation => @annotation}
-            render json: result
-        else
-            render json: created_annotation.errors.full_messages, status: 422
-        end
+  def create
+    annotation = Annotation.new(annotation_params)
+    if annotation.save
+      @annotation = annotation.as_json
+      @annotation[:annotator_name] = annotation.annotator.username
+      @annotation[:votes] = {}
+      broadcast_annotation(annotation, 'POST')
+
+      result = {:annotation => @annotation}
+      render json: result
+    else
+      render json: annotation.errors.full_messages, status: 422
     end
+  end
 
-    def update
-        updated_annotation = Annotation.find(params[:id])
-        if updated_annotation.update(annotation_params)
-            @annotation = updated_annotation.as_json
-            @annotation[:annotator_name] = updated_annotation.annotator.username
-            @annotation[:votes] = {}
-            updated_annotation.votes.each do |vote|
-                @annotation[:votes][vote.id] = vote.slice(:id, :voteable_id, :voteable_type, :voter_id)
-            end
+  def update
+    annotation = Annotation.find(params[:id])
+    if annotation.update(annotation_params)
+      @annotation = annotation.as_json
+      @annotation[:annotator_name] = annotation.annotator.username
+      @annotation[:votes] = {}
+      annotation.votes.each do |vote|
+        @annotation[:votes][vote.id] = vote.slice(:id, :voteable_id, :voteable_type, :voter_id)
+      end
+      broadcast_annotation(annotation, 'PUT')
 
-            result = {:annotation => @annotation}
-            render json: result
-        else
-            render json: updated_annotation.errors.full_messages, status: 422
-        end
+      result = {:annotation => @annotation}
+      render json: result
+    else
+      render json: annotation.errors.full_messages, status: 422
     end
+  end
 
-    def destroy
-        annotation = Annotation.find(params[:id])
-        if annotation
-            annotation.destroy
-        else
-            render json: ['The annotation does not exist.']
-        end
+  def destroy
+    annotation = Annotation.find(params[:id])
+    if annotation
+      annotation.destroy
+      broadcast_annotation(annotation, 'DELETE')
+    else
+      render json: ['The annotation does not exist.']
     end
+  end
 
-    private
-    def annotation_params
-        params.require(:annotation).permit(:annotator_id, :body, :end_index, :start_index, :track_id)
-    end
+  private
+  def annotation_params
+    params.require(:annotation).permit(:annotator_id, :body, :end_index, :start_index, :track_id)
+  end
 end
