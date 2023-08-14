409b682f396ec1f129fce4c90877f560de1f556c
thedaebu <min.kim163@gmail.com>
2023-08-14 11:46:21 -0400

    remove cableApp prop for component render

--- frontend/__test__/components/flash_message/flash_message.test.tsx
+++ frontend/__test__/components/flash_message/flash_message.test.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { cleanup, screen, within } from "@testing-library/react";
-import actionCable, { Cable } from "actioncable";
 import { renderNonShowComponentWithoutUser } from "../../test_store_data";
 import App from "../../../components/app";
-
-const cable: Cable = actionCable.createConsumer(`ws://${window.location.hostname}:3000/cable`);
-const cableApp: { cable: Cable } = { cable };
 
 describe("flash message", () => {
     beforeEach(() => {
@@ -15,13 +11,13 @@ describe("flash message", () => {
         jest.clearAllTimers();
     });
     test("does not display flash message initially", () => {
-        renderNonShowComponentWithoutUser(<App cableApp={ cableApp } />);
+        renderNonShowComponentWithoutUser(<App />);
         const flashMessage = screen.queryByTestId("flash-message")
         expect(flashMessage).not.toBeInTheDocument();
         cleanup();
     });
     test("displays flash message component with correct message when there is a message in the redux state", () => {
-        renderNonShowComponentWithoutUser(<App cableApp={ cableApp } />, "Test Flash Message.");
+        renderNonShowComponentWithoutUser(<App />, "Test Flash Message.");
         const flashMessage = screen.queryByTestId("flash-message");
         expect(flashMessage).toBeInTheDocument();
         const flashMessageMessage = within(flashMessage).queryByTestId("flash-message__message");
