ccbffa3f01db88fbb3e853525101719bffcdbee7
thedaebu <min.kim163@gmail.com>
2023-08-18 12:34:05 -0400

    update import for Root

--- frontend/entry.tsx
+++ frontend/entry.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureAppStore, { Store } from "./store/store";
-import Root from "./components/root";
+import Root from "./components/Root";
 import { User, Window } from "./my_types";
 
 declare const window: Window;
@@ -9,10 +9,10 @@ declare const window: Window;
 document.addEventListener("DOMContentLoaded", () => {
     let store: Store;
     if (window.currentUser) {
-        const preloadedState: {entities: {user: User}} = {
+        const preloadedState: { entities: { user: User; }; } = {
           entities: {
             user: window.currentUser
-          },
+          }
         };
         store = configureAppStore(preloadedState);
         delete window.currentUser;
