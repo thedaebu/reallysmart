f82794b986a13107fcf6a029b07e198fd868e6c2
thedaebu <min.kim163@gmail.com>
2023-08-25 13:45:08 -0400

    add delete funtionality for account item

--- frontend/components/account/AccountComments.tsx
+++ frontend/components/account/AccountComments.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { MouseEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import * as AccountActions from "./../../actions/account_actions";
+import * as CommentActions from "./../../actions/comment_actions";
+import { AnyAction, Dispatch } from "@reduxjs/toolkit";
 import { AccountComment } from "../../my_types";
 
-function AccountComments({ comments }: { comments: Array<AccountComment>; }) {
-    function commentItem(comment: AccountComment, idx: number) {
-        const { body, commentable_body, commentable_type, created_at, track, votes } = comment;
+function AccountComments({ comments, currentUserId }: { comments: Array<AccountComment>; currentUserId: number }) {
+    const [deleteStatuses, setDeleteStatuses] = useState<Set<number>>(new Set());
+
+    const dispatch: Dispatch<AnyAction> = useDispatch();
+    const deleteComment: Function = (id: number) => dispatch(CommentActions.deleteComment(id));
+    const fetchAccount: Function = (id: number) => dispatch(AccountActions.fetchAccount(id));
+
+    function commentItem(comment: AccountComment) {
+        const { body, commentable_body, commentable_type, created_at, id, track, votes } = comment;
         const { artist, title } = track;
 
         return (
             <li
                 className="account-item"
-                key={idx}
+                key={id}
                 data-testid="account-item"
             >
                 <p>{body}<span className="account-item__votes"> +{votes}</span></p>
@@ -21,6 +31,7 @@ function AccountComments({ comments }: { comments: Array<AccountComment>; }) {
                     </span>
                 </Link>
                 <time className="account-item__date">{` - ${dateDisplay(created_at)}`}</time>
+                {deleteDisplay(id)}
             </li>
         );
     }
@@ -42,11 +53,63 @@ function AccountComments({ comments }: { comments: Array<AccountComment>; }) {
         return `${year}-${month}-${day}`;
     }
 
+    function deleteDisplay(id: number) {
+        return deleteStatuses.has(id) ? (
+            <>
+                <span className="account-item__question">Are you sure?</span>
+                <button
+                    className="account-item__button"
+                    onClick={handleDeleteSubmit}
+                    data-id={id}
+                >
+                    Yes
+                </button>
+                <button
+                    className="account-item__button"
+                    onClick={handleDeleteStatus}
+                    data-id={id}
+                >
+                    No
+                </button>
+            </>
+        ) : (
+            <button
+                className="account-item__button"
+                onClick={handleDeleteStatus}
+                data-id={id}
+            >
+                Delete
+            </button>
+        );
+    }
+
+    function handleDeleteSubmit(e: MouseEvent<HTMLButtonElement>) {
+        e.preventDefault();
+
+        const id: number = parseInt(e.currentTarget.dataset.id);
+        deleteComment(id)
+            .then(() => {
+                setDeleteStatuses((statuses: Set<number>) => new Set([...statuses].filter((status: number) => status !== id)));
+                fetchAccount(currentUserId);
+            });
+    }
+
+    function handleDeleteStatus(e: MouseEvent<HTMLButtonElement>) {
+        e.preventDefault();
+
+        const id: number = parseInt(e.currentTarget.dataset.id);
+        if (deleteStatuses.has(id)) {
+            setDeleteStatuses((statuses: Set<number>) => new Set([...statuses].filter((status: number) => status !== id)));
+        } else {
+            setDeleteStatuses((statuses: Set<number>) => new Set([...statuses, id]));
+        }
+    }
+
     return (
         <ul>
             <h1 className="account-show__h1">Comments</h1>
-            {comments.map((comment: AccountComment, idx: number) => (
-                commentItem(comment, idx)
+            {comments.map((comment: AccountComment) => (
+                commentItem(comment)
             ))}
         </ul>
     );
