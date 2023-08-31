efbdd1727e600c5651427362dfef6aca901858b2
thedaebu <min.kim163@gmail.com>
2023-08-31 15:15:57 -0400

    change default value for preLoadedState argument

--- frontend/store/store.ts
+++ frontend/store/store.ts
@@ -2,7 +2,7 @@ import { configureStore, Middleware } from "@reduxjs/toolkit";
 import rootReducer from "../reducers/root_reducer";
 import { User } from "../my_types";
 
-function configureAppStore(preloadedState: { entities: { user: User; }; } = null) {
+function configureAppStore(preloadedState: { entities: { user: User; }; } | {} = {}) {
     const middleware: Array<Middleware> = [];
     // if (process.env.NODE_ENV === "development") {
     //     const { logger } = require("redux-logger");
