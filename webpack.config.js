3aaae57f160251b208258c98b23827ad99c75944
thedaebu <min.kim163@gmail.com>
2023-09-28 13:19:53 -0400

    edit for indentation and es6 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path');
+import { resolve } from 'path';
 
 module.exports = {
-  context: __dirname,
-  entry: "./frontend/entry",
-  output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        }
-      },
-      {
-        test: /\.(ts|tsx)?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-            happyPackMode: true,
-            configFile: path.resolve(__dirname, 'tsconfig.json')
-          }
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"]
-  }
+    context: __dirname,
+    entry: './frontend/entry',
+    output: {
+        path: resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            },
+            {
+                test: /\.(ts|tsx)?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                        happyPackMode: true,
+                        configFile: resolve(__dirname, 'tsconfig.json')
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }
 };
